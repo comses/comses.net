8b66ccf752917488a4b58c257e3340b6e0fc58c6
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-02-02 12:29:31 -0700

    Added missing build folder in frontend

---
+++ frontend/build/webpack.dev.conf.js
@@ -0,0 +1,28 @@
+var config = require('../config')
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var utils = require('./utils')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  // eval-source-map is faster for development
+  devtool: '#eval-source-map',
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    //new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ]
+})
