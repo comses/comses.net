bc86ea06402cb8d5b7aadac6e449ede69a9faee1
Allen Lee <allen.lee@asu.edu>
2017-02-09 20:50:45 -0700

    add travis build with 0% coverage
    - fix pytest docker-compose entry
    - simplify test.sh logic

--- deploy/app/test.sh
+++ deploy/app/test.sh
@@ -2,7 +2,4 @@
 
 /bin/sh /code/deploy/app/common.sh;
 /code/deploy/app/wait-for-it.sh db:5432 -- invoke initialize_database_schema;
-invoke coverage;
-if [[ $RUN_COVERALLS ]]; then
-    coveralls
-fi
+invoke coverage && $RUN_COVERALLS && coveralls
