a81c9e3a8da62743912ca29dcb8f579e0e94bb77
Allen Lee <allen.lee@asu.edu>
2017-02-22 21:32:46 -0700

    add bootstrap to exported vendors js bundle

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -14,7 +14,8 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 module.exports = {
     entry: {
         app: './src/main.ts',
-        styles: './src/style.ts'
+        vendors: './src/vendors.js',
+        styles: './src/style.scss'
     },
     output: {
         path: config.build.assetsRoot,
@@ -67,7 +68,7 @@ module.exports = {
                 include: projectRoot,
                 exclude: /node_modules/,
                 options: {
-                    // 5. Append a ".ts" file to all ".vue" file thus typescript can preprocess the file
+                    // 5. Append a ".ts" file to all ".vue" file so typescript can preprocess the file
                     appendTsSuffixTo: [/\.vue$/]
                 }
             },
@@ -111,23 +112,13 @@ module.exports = {
         utils.extractSCSS,
         // split vendor js into its own file
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: function (module, count) {
-                // any required modules inside node_modules are extracted to vendor
-                return (
-                    module.resource &&
-                    /\.js$/.test(module.resource) &&
-                    module.resource.indexOf(
-                        path.join(__dirname, '../node_modules')
-                    ) === 0
-                )
-            }
+            name: 'vendors'
         }),
         // extract webpack runtime and module manifest to its own file in order to
         // prevent vendor hash from being updated whenever app bundle is updated
         new webpack.optimize.CommonsChunkPlugin({
             name: 'manifest',
-            chunks: ['vendor']
+            chunks: ['vendors']
         })
     ]
 };
