1ef52cb155ef418d6a6b5b06ef47d295dc2bb6d5
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-03-08 16:00:04 -0700

    Added permissions to update and create job forms

--- django/core/view_helpers.py
+++ django/core/view_helpers.py
@@ -1,21 +1,33 @@
 from django.views.generic import TemplateView
 from django.conf.urls import url
 import os
+from django.contrib.auth.decorators import login_required
+from guardian.decorators import permission_required_or_403
 
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def create_edit_routes(base_name: str, prefix: str, lookup_field: str, lookup_regex: str, app_name: str):
+def create_edit_routes(prefix: str, model, lookup_field: str, lookup_regex: str):
     base_url = r'{prefix}/(?P<{lookup_field}>{lookup_regex})/'.format(prefix=prefix, lookup_field=lookup_field,
                                                                         lookup_regex=lookup_regex)
     update_form_url = base_url + 'update/'
     create_form_url = prefix + '/create/'
+
+    app_name = model._meta.app_label
+    base_name = model._meta.object_name.lower()
     template_name = os.path.join(app_name, prefix, 'create_or_update.jinja')
+
+    perm = app_name + '.change_' + base_name
+
+    create_view = login_required()(TemplateView.as_view(template_name=template_name))
+    update_view = permission_required_or_403(perm, (model, lookup_field, lookup_field))(
+        TemplateView.as_view(template_name=template_name))
+
     return [
-        url(update_form_url, TemplateView.as_view(template_name=template_name),
+        url(update_form_url, create_view,
             name='{base_name}-update'.format(base_name=base_name)),
-        url(create_form_url, TemplateView.as_view(template_name=template_name),
+        url(create_form_url, update_view,
             name='{base_name}-create'.format(base_name=base_name))
     ]
--- django/home/urls.py
+++ django/home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from . import views
+from . import models
 from rest_framework.routers import SimpleRouter
 from core.view_helpers import create_edit_routes
 
@@ -12,13 +13,13 @@ router.register(r'tags', views.TagViewSet, base_name='tag')
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home/index.jinja'), name='index'),
     url(r'^', include(router.urls)),
-    url(r'^resources', TemplateView.as_view(template_name='home/resources.jinja'),
+    url(r'^resources/$', TemplateView.as_view(template_name='home/resources.jinja'),
         name='resources'),
-    url(r'^community', TemplateView.as_view(template_name='home/community.jinja'), name='community'),
+    url(r'^community/$', TemplateView.as_view(template_name='home/community.jinja'), name='community'),
 ]
 
 urlpatterns += router.urls
 
-edit_route_form_data = {'lookup_field': 'pk', 'lookup_regex': r'\d+', 'app_name': 'home'}
-for base_name in ['job', 'event']:
-    urlpatterns += create_edit_routes(base_name=base_name, prefix=base_name + 's', **edit_route_form_data)
+edit_route_form_data = {'lookup_field': 'pk', 'lookup_regex': r'\d+'}
+for model in [models.Job, models.Event]:
+    urlpatterns += create_edit_routes(prefix=model._meta.object_name.lower() + 's', model=model, **edit_route_form_data)
