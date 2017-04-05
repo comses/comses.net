e178c699978c65e3c394101bcf9da5cf3efe230d
Allen Lee <allen.lee@asu.edu>
2017-04-05 14:30:26 -0700

    minor refactor
    - use textwrap.shorten instead of manual ellipsizing
    - summarize_to_text doesn't do very well with non-Markdown content, more
      refactoring needed for it to be broadly useful

--- django/core/summarization.py
+++ django/core/summarization.py
@@ -1,11 +1,13 @@
-from sumy.summarizers.luhn import LuhnSummarizer
-from sumy.summarizers.lsa import LsaSummarizer
-from sumy.parsers.html import HtmlParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
-from .utils import markdown_to_sanitized_html
 import jinja2
+
+from sumy.nlp.stemmers import Stemmer
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.parsers.html import HtmlParser
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.utils import get_stop_words
+from textwrap import shorten
+
+from .utils import markdown_to_sanitized_html
 
 LANGUAGE = 'english'
 SENTENCES_COUNT = 6
@@ -28,10 +30,10 @@ def summarize(md: str, sentences_count=5):
     return summary
 
 
-def summarize_to_text(md: str, sentences_count):
+def summarize_to_text(md: str, sentences_count, max_length=300):
     sanitized_html = markdown_to_sanitized_html(md)
     parser = HtmlParser.from_string(sanitized_html, url=None, tokenizer=Tokenizer(LANGUAGE))
     summary = '\n\n'.join(' '.join(sentence.words) for sentence in summarizer(parser.document, sentences_count))
-    if len(summary) >= 300:
-        summary = summary[:296] + '...'
+    if max_length is not None:
+        return shorten(summary, width=max_length)
     return summary
