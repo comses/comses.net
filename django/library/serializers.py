e71805181a8e50453d5b7b47ae4f0d8771a9c41f
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-04-10 15:12:35 -0700

    [skip ci] Change `absolute_url` serializer fields to be URLFields

--- django/library/serializers.py
+++ django/library/serializers.py
@@ -28,14 +28,11 @@ class CodebaseContributorSerializer(serializers.ModelSerializer):
 
 class CodebaseReleaseSerializer(serializers.ModelSerializer):
     date_created = serializers.DateTimeField(format='%Y-%m-%d', read_only=True)
-    absolute_url = serializers.SerializerMethodField(help_text=_('URL to the detail page of the codebase'))
+    absolute_url = serializers.URLField(source='get_absolute_url', help_text=_('URL to the detail page of the codebase'))
     codebase_contributors = CodebaseContributorSerializer(many=True)
     submitter = home_serializers.UserSerializer(label='Submitter')
     platforms = home_serializers.TagSerializer(many=True)
     programming_languages = home_serializers.TagSerializer(many=True)
-
-    def get_absolute_url(self, obj) -> str:
-        return obj.get_absolute_url()
 
     class Meta:
         model = CodebaseRelease
@@ -50,7 +47,7 @@ class CodebaseSerializer(serializers.ModelSerializer):
     date_created = serializers.DateTimeField(format='%Y-%m-%d', read_only=True)
     last_modified = serializers.DateTimeField(format='%Y-%m-%d', read_only=True)
     tags = home_serializers.TagSerializer(many=True)
-    absolute_url = serializers.SerializerMethodField(help_text=_('URL to the detail page of the codebase'))
+    absolute_url = serializers.URLField(source='get_absolute_url')
     submitter = home_serializers.CreatorSerializer(read_only=True)
 
     @staticmethod
@@ -59,9 +56,6 @@ class CodebaseSerializer(serializers.ModelSerializer):
             return obj.summary
         else:
             return shorten(obj.description, width=500)
-
-    def get_absolute_url(self, obj) -> str:
-        return obj.get_absolute_url()
 
     def create(self, validated_data):
         return home_serializers.create(self.Meta.model, validated_data, self.context)
