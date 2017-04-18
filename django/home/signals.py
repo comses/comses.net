475c9e8f35097f21d61575dc25512c5b6a2c5342
Allen Lee <allen.lee@asu.edu>
2017-04-17 21:48:42 -0700

    keep django.contrib Site in sync with Wagtail Site
    django.contrib Site defers to the Wagtail Site's changes.

--- django/home/signals.py
+++ django/home/signals.py
@@ -1,11 +1,38 @@
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from wagtail.wagtailcore.models import Site as WagtailSite
 
 from .models import MemberProfile
 
 
 @receiver(post_save, sender=User, dispatch_uid='member_profile_creator')
-def create_member_profile(sender, instance, created, **kwargs):
+def create_member_profile(sender, instance: User, created, **kwargs):
+    """
+    Ensures every created User has an associated MemberProfile as well
+    :param sender: 
+    :param instance: 
+    :param created: 
+    :param kwargs: 
+    :return: 
+    """
     if created:
         MemberProfile.objects.get_or_create(user=instance)
+
+
+@receiver(post_save, sender=WagtailSite, dispatch_uid='wagtail_site_sync')
+def sync_wagtail_sites(sender, instance: WagtailSite, created : bool, **kwargs):
+    """
+    Keeps default django.contrib.sites.models.Site in sync with the  richer wagtail.wagtailcore.models.Site instance.
+    :param sender: 
+    :param instance: 
+    :param created: 
+    :param kwargs: 
+    :return: 
+    """
+    if instance.is_default_site and all(instance.site_name, instance.hostname):
+        site = Site.objects.first()
+        site.name = instance.site_name
+        site.domain = instance.hostname
+        site.save()
