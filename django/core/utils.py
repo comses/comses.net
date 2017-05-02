a8b6118e11729e36c2c4288d98f5bb21a48a8af5
Allen Lee <allen.lee@asu.edu>
2017-05-02 11:12:41 -0700

    add default markdown extensions to conversion
    - extra is a bundle of PHP Markdown enhancements
    - see https://pythonhosted.org/Markdown/extensions/ for details

--- django/core/utils.py
+++ django/core/utils.py
@@ -1,10 +1,24 @@
-import markdown
 import bleach
+import markdown
 
-ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3', 'pre', 'br']
+ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['p', 'h1', 'h2', 'h3', 'h4', 'pre', 'br', 'div', 'span']
+
+DEFAULT_MARKDOWN_EXTENSIONS = [
+    'markdown.extensions.extra',
+    'markdown.extensions.codehilite',
+    'markdown.extensions.nl2br',
+    'markdown.extensions.sane_lists',
+    'markdown.extensions.smarty',
+    'markdown.extensions.toc',
+#    'markdown.extensions.wikilinks',
+]
 
 
-def markdown_to_sanitized_html(md: str):
-    html = markdown.markdown(md)
-    sanitized_html = bleach.clean(bleach.linkify(html), tags=ALLOWED_TAGS)
-    return sanitized_html
+def markdown_to_sanitized_html(md: str, extensions=None):
+    if extensions is None:
+        extensions = DEFAULT_MARKDOWN_EXTENSIONS
+    html = markdown.markdown(
+        md,
+        extensions=extensions
+    )
+    return bleach.clean(bleach.linkify(html), tags=ALLOWED_TAGS)
