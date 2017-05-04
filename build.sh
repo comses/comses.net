750fb02bbc4dc1dde14598f4fc2be7cae61206bd
Allen Lee <allen.lee@asu.edu>
2017-05-04 12:28:31 -0700

    add clean function to build script
    - add marginally better practices for bash scripting culled from
    https://github.com/kvz/bash3boilerplate

--- build.sh
+++ build.sh
@@ -1,33 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# invoke via `DEPLOY=prod ./build.sh`
+
+set -o errexit
+set -o pipefail
+set -o nounset
 
 # Adapted from http://lukeswart.net/2016/03/lets-deploy-part-1/
 
-DEPLOY=${DEPLOY:-"dev"} # or prod
+function clean()
+{
+    cat "${CONFIG_INI}"; 
+    cp "${CONFIG_INI}" "${BACKUP_CONFIG_INI}";
+    echo "Deleting all docker data in ./docker/shared/"
+    sudo rm -rf ./docker/shared;
+}
+
+DEPLOY=${DEPLOY:-"dev"} # allowed values: (dev | prod)
 DEFAULT_COMPOSE_TEMPLATE="dc-${DEPLOY}.yml"
 DOCKER_COMPOSE_TEMPLATE=${1:-$DEFAULT_COMPOSE_TEMPLATE}
-CONFIG_INI=deploy/conf/config.ini
-BACKUP_CONFIG_INI=/tmp/comsesnet.$RANDOM.ini
+CONFIG_INI=./deploy/conf/config.ini
+BACKUP_CONFIG_INI=/tmp/comsesnet.${RANDOM}.ini
 
 echo "Templating ${DOCKER_COMPOSE_TEMPLATE}"
-echo "For improved security, you should have a 'comses' user with uid/gid of 2718, otherwise you'll need to run root inside the container."
+echo "For improved security, consider creating a local 'comses' user with uid/gid of 2718, otherwise you'll need to run root inside the container."
 
 set -a
-. deploy/conf/docker.env
-cp deploy/conf/docker.env .env
+. ./deploy/conf/docker.env
+cp ./deploy/conf/docker.env .env
 
-if [[ -f "$CONFIG_INI" ]]; then
-    echo "Existing $CONFIG_INI will be overwritten and you may lose access to existing containerized data. Continue?"
+if [[ -f "${CONFIG_INI}" ]]; then
+    echo "Existing ${CONFIG_INI} will be overwritten and all existing containerized data will be removed. Continue?"
     select response in "Yes" "No"; do
-        case "$response" in
-            Yes) echo "Copying existing config.ini to $BACKUP_CONFIG_INI and then overwriting"; cat "$CONFIG_INI"; cp "$CONFIG_INI" "$BACKUP_CONFIG_INI"; break;;
+        case "${response}" in
+            Yes) echo "Copying existing config.ini to ${BACKUP_CONFIG_INI} and then overwriting"; clean; break;;
             No) echo "Aborting build."; exit;;
         esac
     done
 fi
-DB_PASSWORD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c30`
-DJANGO_SECRET_KEY=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c42`
+DB_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c42)
+DJANGO_SECRET_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c42)
 
-cat "$CONFIG_INI".template | envsubst > "$CONFIG_INI"
-cat "${DOCKER_COMPOSE_TEMPLATE}" | envsubst > generated-"$DOCKER_COMPOSE_TEMPLATE"
-ln -sf generated-"$DOCKER_COMPOSE_TEMPLATE" docker-compose.yml
+echo "Running env substitution for DB_PASSWORD ${DB_PASSWORD} and DJANGO_SECRET_KEY ${DJANGO_SECRET_KEY}"
+cat "${CONFIG_INI}".template | envsubst > "${CONFIG_INI}"
+cat "${DOCKER_COMPOSE_TEMPLATE}" | envsubst > generated-"${DOCKER_COMPOSE_TEMPLATE}"
+ln -sf generated-"${DOCKER_COMPOSE_TEMPLATE}" docker-compose.yml
 docker-compose build --pull --force-rm
