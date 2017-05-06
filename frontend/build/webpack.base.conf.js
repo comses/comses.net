b60049babc5170ee3b72c10d4f704a62437ff8bb
Allen Lee <allen.lee@asu.edu>
2017-05-05 23:48:31 -0700

    add Alte-DIN webfont to webpack build and scss
    - downloaded from http://webfonts.ffonts.net/Alte-DIN-1451-Mittelschrift.font
    - add peer reviewed icon/image. Currently includes text, should consider
      cropping that out and putting text manually
    - more work on codebase landing page styling
    
    [skip ci]

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -94,7 +94,8 @@ module.exports = {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 10000,
+                    limit: 50000,
+                    mimetype: 'application/font-woff',
                     name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
                 }
             }
