c09d0572bf8154c6878e5f4e1860b5493df1284f
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-05-11 14:03:27 -0700

    Added v-validate directive and type as optional props to c-input component

--- frontend/src/components/forms/input.vue
+++ frontend/src/components/forms/input.vue
@@ -1,7 +1,8 @@
 <template>
     <div :class="['form-group', {'has-danger': hasDanger }]">
         <slot name="label"></slot>
-        <input type="text" :name="name" class="form-control" :value="value" @input="updateValue($event.target.value)">
+        <input :type="type" :name="name" class="form-control" :value="value" v-validate="validate"
+               @input="updateValue($event.target.value)">
         <div class="form-control-feedback form-control-danger">
             {{ errorMessage }}
         </div>
@@ -13,5 +14,11 @@
     import BaseControl from './base'
 
     @Component
-    export default class Input extends BaseControl {}
+    export default class Input extends BaseControl {
+        @Prop({ default: ''})
+        validate: string;
+
+        @Prop({ default: 'text'})
+        type: string;
+    }
 </script>
