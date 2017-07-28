e909ed1585dc2877d457d041c79d3233d266a12a
Allen Lee <allen.lee@asu.edu>
2017-07-28 13:27:22 -0700

    initialize schema in bash function

--- django/deploy/test.sh
+++ django/deploy/test.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 chmod a+x /code/deploy/*.sh;
-cd /code;
-/code/deploy/wait-for-it.sh db:5432 -- invoke initialize_database_schema;
+
+initdb() {
+    cd /code;
+    echo "Destroying and initializing database from scratch"
+    /code/deploy/wait-for-it.sh db:5432 -- invoke initialize_database_schema
+}
+initdb
 /code/deploy/wait-for-it.sh elasticsearch:9200 -t 30 -- invoke test --tests="$@" --coverage
