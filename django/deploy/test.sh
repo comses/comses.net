07b50e6708d5e478ae05b6c9265ce08943f7dd05
Allen Lee <allen.lee@asu.edu>
2017-08-01 16:43:10 -0700

    more tweaks to the build

--- django/deploy/test.sh
+++ django/deploy/test.sh
@@ -5,7 +5,7 @@ chmod a+x /code/deploy/*.sh;
 initdb() {
     cd /code;
     echo "Destroying and initializing database from scratch"
-    /code/deploy/wait-for-it.sh db:5432 -- invoke initialize_database_schema
+    /code/deploy/wait-for-it.sh db:5432 -- invoke idb
 }
 initdb
 /code/deploy/wait-for-it.sh elasticsearch:9200 -t 30 -- invoke test --tests="$@" --coverage
