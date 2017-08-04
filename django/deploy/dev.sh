454070eb08c9d6bd240fbdce17b9a7c85bd90884
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-04 16:46:48 -0700

    Updated dev script to use new name

--- django/deploy/dev.sh
+++ django/deploy/dev.sh
@@ -9,10 +9,10 @@ initdb() {
     cd /code;
     if [ "$CLEAN_DATABASE" = "true" ]; then
         echo "Destroying and initializing database from scratch"
-        /code/deploy/wait-for-it.sh db:5432 -- invoke initialize_database_schema --clean
+        /code/deploy/wait-for-it.sh db:5432 -- invoke idb --clean
     else
         echo "Using existing db schema"
-        /code/deploy/wait-for-it.sh db:5432 -- invoke initialize_database_schema
+        /code/deploy/wait-for-it.sh db:5432 -- invoke idb
     fi
 }
 initdb
