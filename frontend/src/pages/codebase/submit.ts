1ed3ff05acb0014683fea26a26b77b3ed7c093fb
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-03 17:34:05 -0700

    Removed terms and conditions check on submission

--- frontend/src/pages/codebase/submit.ts
+++ frontend/src/pages/codebase/submit.ts
@@ -8,11 +8,6 @@ import * as yup from 'yup'
 
 @Component({
     template: `<div>
-            <c-checkbox v-model="acceptTermsAndConditions" label="Do you accept terms and conditions of archiving your model at CoMSES?">
-                <template slot="label" scope="props">
-                    {{ props.label }}
-                </template>
-            </c-checkbox>
             <button class="btn btn-primary" type="button" @click="submit">Submit</button>
             <div class="alert alert-danger" role="alert" v-if="validationMsg.type == 'error'">
                 {{ validationMsg.msg }}
@@ -28,7 +23,6 @@ import * as yup from 'yup'
     }
 })
 export default class Submit extends Vue {
-    acceptTermsAndConditions: boolean = false;
     validationMsg: { type: 'error' | 'success' | 'none', msg: string} = { type: 'none', msg: ''};
 
     @Watch('acceptTermsAndConditions')
@@ -36,24 +30,9 @@ export default class Submit extends Vue {
         this.validationMsg = { type: 'none', msg: ''};
     }
 
-    test() {
-        const schema = yup.object().shape({
-            name: yup.string().required(),
-            age: yup.string().required()
-        })
-        schema.validate({ name: ''}, {abortEarly: false}, function(err, value) {
-            console.log({schema, err, value});
-        })
-    }
-
     submit() {
-        this.test();
-        if (this.acceptTermsAndConditions) {
-            this.$store.dispatch('submit')
-                .then(response => this.validationMsg = { type: 'success', msg: 'Submission Successful'})
-                .catch(response => this.validationMsg = { type: 'error', msg: 'Submission Failed' });
-        } else {
-            this.validationMsg = {type: 'error', msg: 'In order to submit a model you must accept the terms and conditions'}
-        }
+        this.$store.dispatch('submitIfValid')
+            .then(response => this.validationMsg = { type: 'success', msg: 'Submission Successful'})
+            .catch(response => this.validationMsg = { type: 'error', msg: 'Submission Failed' });
     }
 }
