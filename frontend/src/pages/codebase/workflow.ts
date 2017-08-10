4467a574a292a082f6f2cc268a4e333fc6881092
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-10 13:38:28 -0700

    Removed submit and codebase pages from navigation

--- frontend/src/pages/codebase/workflow.ts
+++ frontend/src/pages/codebase/workflow.ts
@@ -10,7 +10,7 @@ import Contributors from './contributors'
 import Submit from './submit'
 import Upload from './upload.vue'
 import CodebaseMetadata from './codebase_metadata'
-import CodebaseReleaseMetadata from './codebase_release_metadata'
+import CodebaseReleaseMetadata from './detail'
 import { store, exposeComputed } from './store'
 
 Vue.use(Vuex);
@@ -23,39 +23,36 @@ const component = {
 @Component(<any>{
     store : new Vuex.Store(store),
     template: `<div>
-        <h1>{{ $store.state.release.codebase.title }} <i>v{{ $store.state.release.version_number }}</i></h1>
-        <ul class="nav">
-            <li class="nav-item">
-                <router-link :to="{ name: 'codebase'}" class="nav-link" active-class="disabled">Model Metadata</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'code_upload'}" class="nav-link" active-class="disabled">Upload Code</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'data_upload'}" class="nav-link" active-class="disabled">Upload Data</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'documentation_upload'}" class="nav-link" active-class="disabled">Upload Documentation</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'contributors' }" class="nav-link" active-class="disabled">Contributors</router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'detail' }" class="nav-link" active-class="disabled">
-                    Detail<span class="badge badge-pill badge-danger" v-if="detailPageErrors !== 0">{{ detailPageErrors }} errors</span>
-                </router-link>
-            </li>
-            <li class="nav-item">
-                <router-link :to="{ name: 'submit' }" class="nav-link" active-class="disabled">Submit</router-link>
-            </li>
-        </ul>
-        <router-view></router-view>
-        <!--<button class="btn btn-primary" type="button" @click="createOrSubmit">Submit</button>-->
+        <div v-if="isInitialized">
+            <h1>{{ $store.state.release.codebase.title }} <i>v{{ $store.state.release.version_number }}</i></h1>
+            <ul class="nav">
+                <li class="nav-item">
+                    <router-link :to="{ name: 'code_upload'}" class="nav-link" active-class="disabled">Upload Code</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'data_upload'}" class="nav-link" active-class="disabled">Upload Data</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'documentation_upload'}" class="nav-link" active-class="disabled">Upload Documentation</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'contributors' }" class="nav-link" active-class="disabled">Contributors</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'detail' }" class="nav-link" active-class="disabled">
+                        Detail<span class="badge badge-pill badge-danger" v-if="detailPageErrors !== 0">{{ detailPageErrors }} errors</span>
+                    </router-link>
+                </li>
+            </ul>
+            <router-view></router-view>
+        </div>
+        <div v-else>
+            <h1>Loading codebase release metadata...</h1>
+        </div>
     </div>`,
     router: new VueRouter({
         routes: [
-            { path: '/', redirect: { name: 'codebase'}},
-            { path: '/codebase/', component: CodebaseMetadata, name: 'codebase' },
+            { path: '/', redirect: { name: 'code_upload'}},
             { path: '/code_upload/', component: Upload, name: 'code_upload',  
                 props: { 
                     uploadType: 'sources',
@@ -74,18 +71,18 @@ const component = {
                 }},
             { path: '/contributors/', component: Contributors, name: 'contributors'},
             { path: '/detail/', component: CodebaseReleaseMetadata, name: 'detail'},
-            { path: '/submit/', component: Submit, name: 'submit'},
         ]
     })
 })
 class Workflow extends Vue {
+    isInitialized: boolean = false;
+
     get detailPageErrors() {
-        const validation_errors = this.$store.state.validation_errors; 
-        return validation_errors.description.length + validation_errors.embargo_end_date.length;
+        return 0;
     }
  
     created() {
-        this.$store.dispatch('initialize', {identifier: '2274', version_number: '1.0.0'});
+        this.$store.dispatch('initialize', {identifier: '2274', version_number: '1.0.0'}).then(response => this.isInitialized = true);
     }
 
     createOrSubmit() {
