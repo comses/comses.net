c29b9143188067d239f1f1efbc442149673ad1a3
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-10 11:42:51 -0700

    Made input-class value expected string type

--- frontend/src/components/forms/datepicker.vue
+++ frontend/src/components/forms/datepicker.vue
@@ -2,7 +2,7 @@
     <div :class="['form-group', {'has-danger': hasDanger}]">
         <slot name="label"></slot>
         <datepicker :value="value" @input="updateValue" wrapper-class="input-group"
-                    :input-class="{'form-control': true, 'form-control-danger': hasDanger}" :clear-button="clearButton" @cleared="cleared">
+                    :input-class="datepickerInputClass" :clear-button="clearButton" @cleared="cleared">
         </datepicker>
         <div v-if="hasDanger" class="form-control-feedback">{{ errorMessage }}</div>
         <slot name="help"></slot>
@@ -21,6 +21,10 @@
     export default class InputDatepicker extends BaseControl {
         @Prop({default: false})
         clearButton: boolean;
+
+        get datepickerInputClass() {
+            return this.hasDanger ? 'form-control form-control-danger' : 'form-control';
+        }
 
         cleared() {
             this.updateValue(null);
