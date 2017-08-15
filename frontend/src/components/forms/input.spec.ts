806012ab879b493e44c285911f78f4f9a0e45d82
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-14 17:33:03 -0700

    Added Jest for unit testing, removed unused deps

---
+++ frontend/src/components/forms/input.spec.ts
@@ -0,0 +1,14 @@
+import * as Vue from 'vue'
+import Input from './input'
+
+describe('input.ts', () => {
+   it('should initialize correctly', () => {
+       const label = 'Description';
+       const vm = new Vue({
+           el: document.createElement('div'),
+           render: (h) => h(Input, { props: { value: 'foo', label }})
+       });
+       expect(vm.$el.querySelector('div label').textContent).toBe(label);
+       expect((<HTMLInputElement>vm.$el.querySelector('div input')).value).toBe('foo');
+   })
+});
