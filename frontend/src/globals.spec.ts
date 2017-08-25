24e5578be567eb6b515a8d86e842d22f67b30547
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-08-25 10:19:11 -0700

    Adapted global presence test to be able to pass in or out of docker

--- frontend/src/globals.spec.ts
+++ frontend/src/globals.spec.ts
@@ -1,5 +1,5 @@
 describe('globals', () => {
     it('__BASE_URL__ should be set', () => {
-        expect(__BASE_URL__).toBe('http://localhost:8000')
+        expect(['http://localhost:8000', 'http://cms:8000']).toContain(__BASE_URL__)
     });
 });
