f362ce49f96f57ae734c441d96e40f723e2932bd
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-09-11 14:53:11 -0700

    wrapped vue-simplemde component, adjusted z-indices

--- frontend/src/components/forms/markdown.ts
+++ frontend/src/components/forms/markdown.ts
@@ -1,6 +1,6 @@
 import BaseControl from 'components/forms/base'
 import {Component, Prop} from 'vue-property-decorator'
-import * as marked from 'marked'
+import {markdownEditor as MarkdownEditor} from 'vue-simplemde'
 
 enum ViewMode {
     code,
@@ -9,83 +9,29 @@ enum ViewMode {
 }
 
 @Component({
-    template: `<div class="form-group">
-        <slot name="label"></slot>
-        <div class="container-fluid p-0">
-            <div class="btn-group">
-                <button type="button" :class="[buttonStyle, {'active': displayView == viewMode.code }]"
-                        @click="displayView = viewMode.code">
-                    Edit
-                </button>
-                <button type="button" :class="[buttonStyle, {'active': displayView == viewMode.view }]"
-                        @click="displayView = viewMode.view">
-                    Preview
-                </button>
-                <button type="button" :class="[buttonStyle, {'active': displayView == viewMode.code_and_view }]"
-                        @click="displayView = viewMode.code_and_view">
-                    Side by Side
-                </button>
-            </div>
-            <div class="row p-0 row-eq-height">
-                <div :class="codeStyle">
-                    <textarea :class="['markdown-textarea', {'is-invalid': isInvalid }]" :style="{ 'min-height': minHeight }" :value="value"
-                                @input="updateValue($event.target.value)"
-                                debounce="300">
-                    </textarea>
-                </div>
-                <div :class="previewStyle">
-                    <div v-html="markdown" class="preview"></div>
-                </div>
-            </div>
-        </div>
+    template: `<div :class="['form-group', {'child-is-invalid': isInvalid }]">
+        <slot name="label" :label="label">
+            <label class="form-control-label">{{ label }}</label>
+        </slot>
+        <markdown-editor :value="value" @input="updateValue"></markdown-editor>
         <div v-if="isInvalid" class="invalid-feedback">{{ errorMessage }}</div>
-        <slot name="help"></slot>
-    </div>`
+        <slot name="help" :help="help">
+            <small class="form-text text-muted">{{ help }}</small>
+        </slot>
+    </div>`,
+    components: {
+        MarkdownEditor
+    }
 })
 class MarkDown extends BaseControl {
     @Prop({default: '10em'})
     minHeight: string;
 
-    displayView: ViewMode = ViewMode.code_and_view;
-    displayFullScreen: boolean = false;
+    @Prop()
+    label: string;
 
-    fullWidthClass = "p-1 col-12";
-    halfWidthClass = "p-1 col-md-12 col-lg-6";
-    hiddenClass = "hidden-xs-up";
-
-    get viewMode() {
-        return ViewMode;
-    }
-
-    get codeStyle() {
-        switch (this.displayView) {
-            case ViewMode.code:
-                return this.fullWidthClass;
-            case ViewMode.view:
-                return this.hiddenClass;
-            case ViewMode.code_and_view:
-                return this.halfWidthClass;
-        }
-    }
-
-    get previewStyle() {
-        switch (this.displayView) {
-            case ViewMode.code:
-                return this.hiddenClass;
-            case ViewMode.view:
-                return this.fullWidthClass;
-            case ViewMode.code_and_view:
-                return this.halfWidthClass;
-        }
-    }
-
-    get buttonStyle() {
-        return 'btn btn-sm btn-secondary'
-    }
-
-    get markdown() {
-        return marked.parse(this.value, {sanitize: true})
-    }
+    @Prop()
+    help: string;
 }
 
 export default MarkDown;
