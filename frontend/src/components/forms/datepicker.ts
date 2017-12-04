a45eb5c70dfef36b3f0d897daa08df29758f10a6
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-12-04 14:29:42 -0700

    Fixed date display off by one error

--- frontend/src/components/forms/datepicker.ts
+++ frontend/src/components/forms/datepicker.ts
@@ -6,7 +6,7 @@ import * as Datepicker from 'vuejs-datepicker'
 @Component({
     template: `<div class="form-group">
         <slot name="label"></slot>
-        <datepicker :value="value" @input="updateDate" wrapper-class="input-group"
+        <datepicker :value="dateValue" @input="updateDate" wrapper-class="input-group"
                     :input-class="datepickerInputClass" :clear-button="clearButton" @cleared="cleared">
         </datepicker>
         <div v-if="isInvalid" class="invalid-feedback-always">{{ errorMessage }}</div>
@@ -22,6 +22,15 @@ export default class InputDatepicker extends BaseControl {
 
     get datepickerInputClass() {
         return this.isInvalid ? 'form-control is-invalid' : 'form-control';
+    }
+
+    get dateValue() {
+        // Otherwise displayed date is off by one https://github.com/charliekassel/vuejs-datepicker/issues/158
+        const [yearStr, monthStr, dayStr] = this.value.split('-');
+        const year = parseInt(yearStr);
+        const month = parseInt(monthStr) - 1; // month argument is zero indexed so 0 is January
+        const day = parseInt(dayStr.split('T')[0]);
+        return new Date(year, month, day);
     }
 
     updateDate(value) {
