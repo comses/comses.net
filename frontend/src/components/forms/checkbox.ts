3021a2f79de5ac5d1eafebe906a630bb3fe14ed2
Calvin Pritchard <calvin.pritchard@asu.edu>
2017-12-07 10:03:35 -0700

    Required required css class to label of checkbox class

--- frontend/src/components/forms/checkbox.ts
+++ frontend/src/components/forms/checkbox.ts
@@ -4,9 +4,11 @@ import BaseControl from './base'
 @Component({
     template: `<div class="form-check">
         <label :for='controlId' class="form-check-label">
-            <input :id='controlId' type="checkbox" :name="name" :class="['form-check-input', {'is-invalid': isInvalid}, requiredClass]" :value="value" :checked="value === true"
+            <input :id='controlId' type="checkbox" :name="name" :class="['form-check-input', {'is-invalid': isInvalid}]" :value="value" :checked="value === true"
                     @change="toggle($event.target.value)">
-            <slot name="label" :label="label">{{ label }}</slot>
+            <slot name="label" :label="label">
+                <span :class="[requiredClass]">{{ label }}</span>
+            </slot>
         </label>
         <slot name="help"><small :aria-describedby='controlId' class="form-text text-muted">{{ help }}</small></slot>
         <div class="invalid-feedback">
