f45cca540beb9e583466fc13df676e35195f3d3b
Allen Lee <allen.lee@asu.edu>
2018-02-08 14:07:01 -0700

    add explicit ordering to signup form fields
    - place captcha last as typical
    - [skip ci]

--- django/core/forms.py
+++ django/core/forms.py
@@ -13,12 +13,12 @@ class SignupForm(forms.Form):
     error_css_class = 'has-errors'
     required_css_class = 'required'
 
+    field_order = ('username', 'email', 'first_name', 'last_name', 'full_member', 'password1', 'password2', 'captcha')
+
     username = forms.CharField(max_length=200, strip=True, required=True)
     email = forms.EmailField(max_length=200, required=True)
-    first_name = forms.CharField(max_length=200, strip=True, required=False,
-                                 label='Given name')
-    last_name = forms.CharField(max_length=200, strip=True, required=False,
-                                label='Family name')
+    first_name = forms.CharField(max_length=200, strip=True, required=False, label='Given name')
+    last_name = forms.CharField(max_length=200, strip=True, required=False, label='Family name')
     full_member = forms.BooleanField(
         required=False,
         help_text=_("By checking this box, I agree to the rights and responsibilities of CoMSES Net full membership"))
