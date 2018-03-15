dcc1af67a7fe62410a15b27af74577f21c8bc410
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-03-15 12:14:05 -0700

    Added new and updates codebase statistics
    - also fixed filtering so that only public codebases and releases are
      included

--- django/curator/management/commands/curator_statistics.py
+++ django/curator/management/commands/curator_statistics.py
@@ -7,7 +7,7 @@ from dateutil.parser import parse as date_parse
 import pytz
 import logging
 
-from django.db.models import Count
+from django.db.models import Count, F, Max, Prefetch
 
 from library.models import CodebaseReleaseDownload, CodebaseRelease, Codebase
 
@@ -21,13 +21,13 @@ class Command(BaseCommand):
         parser.add_argument('--from', '-f', help='isoformat from date')
         parser.add_argument('--to', '-t', help='isoformat to date', default=None)
         parser.add_argument('--directory', '-d', help='directory to store statistics in', default='/shared/statistics')
-        parser.add_argument('--aggregations', '-a', default='release,codebase,ip',
+        parser.add_argument('--aggregations', '-a', default='release,codebase,ip,new',
                             help='aggregations - comma separated list of release, codebase, ip')
 
     def export_release_download_statistics(self, downloads, dest):
-        releases = CodebaseRelease.objects.filter(id__in=downloads.values_list('release_id', flat=True))\
+        releases = CodebaseRelease.objects.filter(id__in=downloads.values_list('release_id', flat=True)) \
             .prefetch_related('codebase').only('version_number', 'codebase__identifier').in_bulk()
-        results = downloads.values('release_id').annotate(count=Count('*'))
+        results = downloads.values('release_id').annotate(count=Count('*')).order_by('-count')
         with open(dest, 'w', newline='') as f:
             fieldnames = ['url', 'count']
             writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -36,9 +36,9 @@ class Command(BaseCommand):
                 writer.writerow({'url': releases[result['release_id']].get_absolute_url(), 'count': result['count']})
 
     def export_codebase_download_statistics(self, downloads, dest):
-        codebases = Codebase.objects.filter(releases__id__in=downloads.values_list('release_id', flat=True))\
+        codebases = Codebase.objects.filter(releases__id__in=downloads.values_list('release_id', flat=True)) \
             .prefetch_related('releases').only('identifier', 'title').in_bulk()
-        results = downloads.values('release__codebase__id').annotate(count=Count('*'))
+        results = downloads.values('release__codebase__id').annotate(count=Count('*')).order_by('-count')
         with open(dest, 'w', newline='') as f:
             fieldnames = ['url', 'count', 'title']
             writer = csv.DictWriter(f, fieldnames=fieldnames)
@@ -50,13 +50,36 @@ class Command(BaseCommand):
                                  'title': codebase.title})
 
     def export_ip_download_statistics(self, downloads, dest):
-        results = downloads.values('ip_address').annotate(count=Count('*'))
+        results = downloads.values('ip_address').annotate(count=Count('*')).order_by('-count')
         with open(dest, 'w', newline='') as f:
             fieldnames = ['ip_address', 'count']
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
             for result in results.iterator():
                 writer.writerow(result)
+
+    def export_new_and_updated_codebases(self, filters, directory):
+        in_range_releases = CodebaseRelease.objects.filter(**filters)
+        if 'date_created__range' in filters:
+            out_range_releases = CodebaseRelease.objects.exclude(date_created__gte=filters['date_created__range'][0])
+        else:
+            out_range_releases = CodebaseRelease.objects.exclude(**filters)
+        new_codebases = Codebase.objects.public().filter(releases__in=in_range_releases)\
+            .exclude(releases__in=out_range_releases).distinct().order_by('title')
+        updated_codebases = Codebase.objects.public().filter(releases__in=in_range_releases)\
+            .filter(releases__in=out_range_releases).distinct().order_by('title')
+
+        max_dates_bulk = {r['codebase_id']: r['date'] for r in in_range_releases
+            .values('codebase_id').annotate(date=Max('date_created'))}
+        for qs, filename in [(new_codebases, 'new_codebases.csv'), (updated_codebases, 'updated_codebases.csv')]:
+            with open(os.path.join(directory, filename), 'w', newline='') as f:
+                fieldnames = ['url', 'title', 'date']
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+                for codebase in qs.distinct().iterator():
+                    writer.writerow({'url': codebase.get_absolute_url(),
+                                     'title': codebase.title,
+                                     'date': max_dates_bulk[codebase.id]})
 
     def handle(self, *args, **options):
         """
@@ -80,7 +103,8 @@ class Command(BaseCommand):
         directory = options['directory']
 
         os.makedirs(directory, exist_ok=True)
-        downloads = CodebaseReleaseDownload.objects.filter(**filters)
+        downloads = CodebaseReleaseDownload.objects.filter(release__in=CodebaseRelease.objects.public())\
+            .filter(**filters)
         if 'codebase' in aggregations:
             self.export_codebase_download_statistics(downloads,
                                                      dest=os.path.join(directory, 'codebase_download_counts.csv'))
@@ -90,3 +114,5 @@ class Command(BaseCommand):
         if 'ip' in aggregations:
             self.export_ip_download_statistics(downloads,
                                                dest=os.path.join(directory, 'ip_download_counts.csv'))
+        if 'new' in aggregations:
+            self.export_new_and_updated_codebases(filters=filters, directory=directory)
