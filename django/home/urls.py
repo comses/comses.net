506b7caf1a78dcd0eb3f37891ceb46d4b24e1e6f
Allen Lee <allen.lee@asu.edu>
2018-03-22 17:23:53 -0700

    add comses digest newsletter to community subnav
    still need to add manually to the community CategoryIndexPage via
    something like
    
    ```
    cip=CategoryIndexPage.objects.get(heading='Community')
    cip.navigation_links.add(SubNavigationLink(title='Newsletter',url='/digest/', sort_order=4))
    ```
    
    closes #278

--- django/home/urls.py
+++ django/home/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from rest_framework.routers import SimpleRouter
 
 from . import views
@@ -10,6 +11,7 @@ router.register(r'jobs', views.JobViewSet, base_name='job')
 router.register(r'users', views.ProfileViewSet, base_name='profile')
 
 urlpatterns = [
+    url(r'^digest/$', TemplateView.as_view(template_name='home/digest.jinja'), name='digest'),
     url(r'^users/follow/$', views.ToggleFollowUser.as_view(), name='follow-user'),
     url(r'^events/(?P<pk>\d+)/edit/$', views.EventUpdateView.as_view(), name='event-edit'),
     url(r'^events/add/$', views.EventCreateView.as_view(), name='event-add'),
