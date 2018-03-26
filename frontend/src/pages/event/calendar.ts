7e325c94dad9aacb149b640ed95fdd979c4ab976
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-03-26 15:58:59 -0700

    Made event calendar title text wrap

--- frontend/src/pages/event/calendar.ts
+++ frontend/src/pages/event/calendar.ts
@@ -3,12 +3,14 @@ import * as $ from 'jquery'
 
 $(function() {
     $('#calendar').fullCalendar({
+        height: 'auto',
         events: {
             'url': '/events/calendar/',
             'allDayDefault': true
         },
         eventRender: function(event, element) {
             let eventElement = element[0];
+            eventElement.innerText = event.title;
             eventElement.setAttribute('data-toggle', 'tooltip');
             eventElement.setAttribute('title', event.title);
         }
