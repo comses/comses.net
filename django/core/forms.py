ba1636f5654c17e8bfb663b874f154a97b2a8fcb
Allen Lee <allen.lee@asu.edu>
2018-04-09 15:46:03 -0700

    fix account creation through social auth
    allauth doesn't appear to add password1 / password2 fields to the
    registration page when signing up for a new account via social auth
    
    [skip ci]

--- django/core/forms.py
+++ django/core/forms.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 class SignupForm(forms.Form):
     error_css_class = 'has-errors'
     required_css_class = 'required'
-
     field_order = ('username', 'email', 'first_name', 'last_name', 'full_member', 'password1', 'password2', 'captcha')
 
     username = forms.CharField(max_length=200, strip=True, required=True)
@@ -21,22 +20,23 @@ class SignupForm(forms.Form):
     last_name = forms.CharField(max_length=200, strip=True, required=False, label='Family name')
     full_member = forms.BooleanField(
         required=False,
-        help_text=_("By checking this box, I agree to the rights and responsibilities of CoMSES Net full membership"))
+        help_text=_("By checking this box, I agree to the rights and responsibilities of CoMSES Net full membership")
+    )
     captcha = ReCaptchaField()
 
     def signup(self, request, user):
         data = self.cleaned_data
         user.username = data['username'].lower()
         user.email = data['email'].lower()
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        password = data['password1']
+        user.first_name = data.get('first_name')
+        user.last_name = data.get('last_name')
+        password = data.get('password1')
         if password:
+            # FIXME: assumes cleaned_data, password1 == password2 check already done
             user.set_password(password)
         else:
             user.set_unusable_password()
-        full_member = data['full_member']
+        full_member = data.get('full_member')
         if full_member:
             user.groups.add(ComsesGroups.FULL_MEMBER.get_group())
-        logger.error("about to save USER %s", user)
         user.save()
