6f7e1e2564419005f58c527435d1f04b873e7fef
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-04-13 14:12:12 -0700

    Added nojekyll and CNAME creation directly to travis.yml

--- django/deploy/build_docs.sh
+++ django/deploy/build_docs.sh
@@ -9,11 +9,3 @@ initdb() {
 }
 initdb
 /code/deploy/wait-for-it.sh elasticsearch:9200 -t 30 -- invoke docs.r
-
-# without this css on gh-pages will not work
-echo "Adding .nojekyll for css"
-touch /code/docs/build/html/.nojekyll
-
-# setup CNAME for DNS https://stackoverflow.com/questions/9082499/custom-domain-for-github-project-pages
-echo "Adding CNAME"
-echo docs.comses.net > /code/docs/build/html/CNAME
