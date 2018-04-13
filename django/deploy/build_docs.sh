3b82f94c6986ad90fb2948a63206468595caec1b
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-04-13 15:10:51 -0700

    Used proper absolute path to nojekyll and CNAME (hopefully)

--- django/deploy/build_docs.sh
+++ django/deploy/build_docs.sh
@@ -9,3 +9,8 @@ initdb() {
 }
 initdb
 /code/deploy/wait-for-it.sh elasticsearch:9200 -t 30 -- invoke docs.r
+
+# Add .nojekyll so that GiutHub will use Sphinx generated CSS
+touch /docs/build/html/.nojekyll
+# Add CNAME for custom domain. See https://stackoverflow.com/questions/9082499/custom-domain-for-github-project-pages
+echo docs.comses.net > /docs/build/html/CNAME
