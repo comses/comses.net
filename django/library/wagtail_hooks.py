fb17a3eb38215b2d726cf55072941875cb57e085
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-04-17 13:08:50 -0700

    Added rough peer review wagtail admin forms
    upgraded to node 9 because yarn was hanging on build

--- django/library/wagtail_hooks.py
+++ django/library/wagtail_hooks.py
@@ -1,6 +1,7 @@
-from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
+from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register, ModelAdminGroup)
 
-from .models import CodebaseImage
+from django.utils.translation import ugettext_lazy as _
+from .models import CodebaseImage, PeerReview, PeerReviewerFeedback, PeerReviewInvitation
 
 
 class CodebaseImageAdmin(ModelAdmin):
@@ -10,3 +11,32 @@ class CodebaseImageAdmin(ModelAdmin):
 
 
 modeladmin_register(CodebaseImageAdmin)
+
+
+class PeerReviewAdmin(ModelAdmin):
+    model = PeerReview
+    list_display = ('status', 'codebase_release__title', 'reviewer_recommendation', 'date_created')
+    list_filter = ('status', 'date_created',)
+    search_fields = ('submitter', 'codebase_release__title')
+
+
+class PeerReviewerFeedbackAdmin(ModelAdmin):
+    model = PeerReviewerFeedback
+    list_display = ('date_created', 'recommendation', 'reviewer')
+    list_filter = ('date_created', 'recommendation', 'reviewer')
+
+
+class PeerReviewInvitationAdmin(ModelAdmin):
+    model = PeerReviewInvitation
+    list_display = ('date_created', 'review__codebase_release', 'candidate_reviewer', )
+    list_filter = ('date_created', 'review__codebase_release', 'candidate_reviewer', )
+
+
+class PeerReviewGroup(ModelAdminGroup):
+    menu_label = 'Peer Review'
+    menu_icon = 'folder-open-inverse'
+    label = _('Peer Review')
+    items = (PeerReviewAdmin, PeerReviewerFeedbackAdmin, PeerReviewInvitationAdmin, )
+
+
+modeladmin_register(PeerReviewGroup)
