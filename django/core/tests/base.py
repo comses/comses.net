97c6715e6ad351d9d47334826f14d356351b61eb
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-04-18 11:08:01 -0700

    Removed all remnants of hypothesis

--- django/core/tests/base.py
+++ django/core/tests/base.py
@@ -3,72 +3,11 @@ import os
 import shlex
 import shutil
 import subprocess
-from urllib.parse import urlencode
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.urls import reverse
-from hypothesis import strategies as st
-from hypothesis.extra import django as hypothesis_django
-from hypothesis.extra.django.models import models, add_default_field_mapping
-
-from core.fields import MarkdownField
 
 logger = logging.getLogger(__name__)
-
-MAX_EXAMPLES = 6
-
-add_default_field_mapping(MarkdownField, st.just("# The description"))
-
-DEFAULT_ALPHABET = st.characters(
-    whitelist_categories={'Lu', 'Ll', 'Lt', 'Lm', 'Lo'},
-    blacklist_characters=(chr(0),)
-)
-
-
-def text(min_size=1, max_size=20):
-    return st.text(alphabet=DEFAULT_ALPHABET, min_size=min_size, max_size=max_size)
-
-
-def dois(**kwargs):
-    return st.text(alphabet=DEFAULT_ALPHABET, **kwargs)
-
-
-def generate_user(username='test_user', password='default.testing.password'):
-    return models(User,
-                  username=st.just(str(username)),
-                  email=st.just(str(username) + "@comses.net"),
-                  first_name=text(),
-                  last_name=text(),
-                  password=st.just(password))
-
-
-class HypothesisTestCase(hypothesis_django.TestCase):
-    def reverse(self, view_name, query_parameters_dict=None):
-        reversed_url = reverse(view_name)
-        if query_parameters_dict is not None:
-            return '%s?%s' % (reversed_url, urlencode(query_parameters_dict))
-        return reversed_url
-
-    def _resolve(self, url=None, view_name=None, query_parameters_dict=None):
-        if view_name is not None:
-            reversed_url = self.reverse(view_name, query_parameters_dict=query_parameters_dict)
-            url = reversed_url
-        if url is None:
-            self.fail("You must pass a URL or view name to access.")
-        return url
-
-    def get(self, *args, url=None, view_name=None, query_parameters_dict=None):
-        url = self._resolve(url, view_name, query_parameters_dict=query_parameters_dict)
-        return self.client.get(url, *args)
-
-    def post(self, *args, url=None, view_name=None, post_data=None):
-        url = self._resolve(url, view_name)
-        return self.client.post(url, post_data, *args)
-
-    def put(self, *args, url=None, view_name=None, put_data=None):
-        url = self._resolve(url, view_name)
-        return self.client.put(url, put_data, *args)
 
 
 def make_user(username='test_user', password='default.testing.password', email='comses.test@mailinator.com'):
