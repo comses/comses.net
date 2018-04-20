3a10b3793cf9d53add1bfb1d53e573fc988d0503
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-04-20 11:31:53 -0700

    Added discourse sso filter for inactive users
    [skip ci]

--- django/core/management/commands/discourse_sso_user_import.py
+++ django/core/management/commands/discourse_sso_user_import.py
@@ -16,12 +16,16 @@ class Command(BaseCommand):
         # these errors should about the email or username already existing or the username being too long
         if not data.get('success', False):
             print('skipping {} {}'.format(user, data.get('errors')))
+            return None
         else:
             print('added user {}'.format(user))
+            return user
 
     def handle(self, *args, **options):
         # getting a list of all users in discourse api is difficult so try to add each one
-        users = User.objects.exclude(username__in=['AnonymousUser', '__test_user__']).order_by('username')
+        users = User.objects.exclude(username__in=['AnonymousUser', '__test_user__']) \
+            .filter(is_active=True).order_by('username')
+        added_users = []
         for user in users:
             print('\nsyncing user {}'.format(user))
             response = create_discourse_user(user)
@@ -29,6 +33,12 @@ class Command(BaseCommand):
                 print('bad reponse for user {} {}'.format(user, response.status_code))
                 print(response.content)
                 continue
-            self.check_discourse_user_response(response, user)
+            user = self.check_discourse_user_response(response, user)
+            if user:
+                added_users.append(user)
             # hack to avoid 429 response from discourse
-            time.sleep(1)
+            time.sleep(0.6)
+
+        print('\nAdded Users')
+        for added_user in added_users:
+            print(added_user)
