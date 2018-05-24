ed1d5f54a377c14afd0bf668a1dadd81d530e107
Calvin Pritchard <calvin.pritchard@asu.edu>
2018-05-24 00:09:10 -0700

    Setup django in invoke tasks

--- django/tasks.py
+++ django/tasks.py
@@ -4,5 +4,7 @@ import sys
 # push current directory onto the path to access core.settings
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
+import django
+django.setup()
 
 from curator.invoke_tasks import ns
