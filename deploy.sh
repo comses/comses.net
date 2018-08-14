5f090c95e4543846ee9a857aa03b142d26a5ecf2
A Lee <alee@users.noreply.github.com>
2018-08-13 19:26:05 -0700

    add 2nd es instance to deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ echo "Deploying latest build of comses.net"
 git describe --tags >| django/release-version.txt
 docker-compose build --pull
 docker-compose pull redis db nginx elasticsearch
-docker-compose up -d cms db nginx redis elasticsearch
+docker-compose up -d cms db nginx redis elasticsearch elasticsearch2
 docker-compose up js
 docker-compose exec cms inv prepare
