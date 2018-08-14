e66dbf5ff209b7ae4d2525341400c1a79a9995f0
Allen Lee <allen.lee@asu.edu>
2018-08-14 02:57:19 +0000

    only bring 2nd es instance on `./deploy.sh prod`

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+ENVIRONMENT=${1:-"staging"}
+
 echo "Deploying latest build of comses.net"
-git describe --tags >| django/release-version.txt
-docker-compose build --pull
-docker-compose pull redis db nginx elasticsearch
-docker-compose up -d cms db nginx redis elasticsearch elasticsearch2
+git describe --tags >| django/release-version.txt;
+docker-compose build --pull;
+docker-compose pull redis db nginx elasticsearch;
+docker-compose up -d cms db nginx redis elasticsearch;
+if [[ ${ENVIRONMENT} == "prod" ]]; then
+    exec docker-compose up -d elasticsearch2
+fi
+
 docker-compose up js
 docker-compose exec cms inv prepare
