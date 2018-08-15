a50dac88a9dfe208253408d859a3e7273d8e0282
Allen Lee <allen.lee@asu.edu>
2018-08-15 14:27:59 -0700

    only start the wait-for-js container in prod

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 ENVIRONMENT=${1:-"dev"}
 
-echo "Deploying latest build of comses.net"
+echo "Deploying latest build of comses.net for **${ENVIRONMENT}**"
 SERVICES="redis db elasticsearch nginx"
 git describe --tags >| django/release-version.txt;
 docker-compose build --pull;
@@ -19,8 +19,8 @@ docker-compose pull ${SERVICES}
 
 if [[ ${ENVIRONMENT} == "prod" ]]; then
     SERVICES="${SERVICES} elasticsearch2"
+    docker-compose up js
 fi
 
 docker-compose up -d cms ${SERVICES};
-docker-compose up js
 docker-compose exec cms inv prepare
