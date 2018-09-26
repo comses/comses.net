0982f2d774cfe155f1422cac861bf1a2465c4e34
Allen Lee <allen.lee@asu.edu>
2018-09-25 20:38:37 -0700

    highlight action needed in review manager UI

--- frontend/src/pages/review/feedback.ts
+++ frontend/src/pages/review/feedback.ts
@@ -11,18 +11,22 @@ const reviewApi = new ReviewEditorAPI();
             <div class="row" v-for="feedback in feedback_items">
                 <div class="col-xs-12 col-sm-6">
                     <a :href="feedback.editor_url">
-                        {{feedback.recommendation}} by {{ feedback.reviewer_name }}
+                        {{ feedback.reviewer_name }} recommended: <mark>{{feedback.recommendation}}</mark>
                     </a>
                 </div>
                 <div class="col-xs-12 col-sm-6">
-                    <span class="badge badge-success" v-if="feedback.reviewer_submitted && feedback.editor_submitted">Review completed and editor has replied to author</span>
+                    <span class="badge badge-success" v-if="feedback.reviewer_submitted && feedback.editor_submitted">
+                        <strong>Review completed</strong> editor has notified author</span>
                     <span class="badge badge-warning" v-if="feedback.reviewer_submitted && !feedback.editor_submitted">
-    Reviewer has submitted a review, editor action needed</span>
-                    <span class="badge badge-primary" v-else>Waiting for reviewer feedback since {{ feedback.date_created }}</span>
+                        <strong>Editor action needed</strong> reviewer feedback available
+                    </span>
+                    <span class="badge badge-primary" v-else>
+                        <strong>Awaiting reviewer feedback</strong> <em>since {{ feedback.date_created }}</em>
+                    </span>
                 </div>
             </div>
         </div>
-        <p v-else>No feedback has been received</p>
+        <p v-else>No reviewer feedback submitted yet</p>
     </div>`
 })
 export class Feedback extends Vue {
