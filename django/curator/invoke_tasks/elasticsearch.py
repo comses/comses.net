8d77a5e9510a46bed3a80cae6311ddb9aa812745
Allen Lee <allen.lee@asu.edu>
2018-10-24 14:45:56 -0700

    add basic license support
    Elasticsearch has updated their basic license API and added an
    indefinite basic license that means we won't need to periodically renew
    a basic license:
    
    https://www.elastic.co/guide/en/elasticsearch/reference/master/start-basic.html
    
    TODO: verify that existing cluster support that we use on prod still
    works with the basic license
    
    [skip ci]

--- django/curator/invoke_tasks/elasticsearch.py
+++ django/curator/invoke_tasks/elasticsearch.py
@@ -1,9 +1,18 @@
+from django.conf import settings
 from invoke import task
 
 
 @task(aliases=['ul'])
-def update_license(ctx, license='/secrets/es5-license.json'):
-    for elasticsearch_host in ('elasticsearch', 'elasticsearch2'):
-        ctx.run(
-            "curl -XPUT 'http://{0}:9200/_xpack/license?acknowledge=true' -H 'Content-Type: application/json' -d @{1}".format(
-                elasticsearch_host, license))
+def update_license(ctx, license_path=None):
+
+    es_hosts = ['elasticsearch']
+    if settings.DEPLOY_ENVIRONMENT.is_production():
+        es_hosts.append('elasticsearch2')
+
+    for elasticsearch_host in es_hosts:
+        if license_path is None:
+            ctx.run("curl -XPOST 'http://{0}:9200/_xpack/license/start_basic?acknowledge=true'".format(elasticsearch_host))
+        else:
+            ctx.run(
+                "curl -XPUT 'http://{0}:9200/_xpack/license?acknowledge=true' -H 'Content-Type: application/json' -d @{1}".format(
+                    elasticsearch_host, license))
