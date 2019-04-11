e5d4bc1ab1151c4163dfe61e9b22e9728c7624e9
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-04-11 14:14:55 -0700

    Added vue cli template

---
+++ frontend-new/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pages: {
+        codebase_detail: {
+            entry: './src/views/codebase/detail.vue'
+        },
+        codebase_list: {
+            entry: './src/views/codebase/list.vue'
+        }
+    }
+};
