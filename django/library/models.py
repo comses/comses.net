c45e2366b4748f9483b46bd3873de32d612d0ded
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-08-22 17:17:09 -0700

    fixed function to verify metadata in codebases since os and programming languages are many-to-many, they should be queried

--- django/library/models.py
+++ django/library/models.py
@@ -920,7 +920,7 @@ class CodebaseRelease(index.Indexed, ClusterableModel):
         return reverse('library:codebaserelease-share-download', kwargs={'share_uuid': self.share_uuid})
 
     def verify_metadata(self):
-        if self.license is None or self.os is None or self.release_notes is None or self.platforms is None or self.programming_languages is None or self.contributors is None:
+        if self.license is None or self.os is None or self.programming_languages.exists() or self.os.exists():
             return False
         else:
             return True
@@ -942,7 +942,7 @@ class CodebaseRelease(index.Indexed, ClusterableModel):
         """
         Returns true if this release has not already been peer reviewed and a related PeerReview does not exist
         """
-        return not self.peer_reviewed and self.get_review() is None and self.verify_metadata() is True
+        return not self.peer_reviewed and self.get_review() is None and self.verify_metadata()
 
     @property
     def is_latest_version(self):
