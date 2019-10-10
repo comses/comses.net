1efa447e0f72827f6ac32e9c91ec487defe65601
Allen Lee <allen.lee@asu.edu>
2019-10-10 00:07:19 +0000

    remove Sentry WSGI wrapper

--- django/core/wsgi.py
+++ django/core/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
-application = Sentry(get_wsgi_application())
+application = get_wsgi_application()
