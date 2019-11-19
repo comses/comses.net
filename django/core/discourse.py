67d2bd43a8e16b53b90fdd313f7ccd20127f7d90
Allen Lee <allen.lee@asu.edu>
2019-11-19 15:28:18 -0700

    replace deprecated query params with http headers
    resolves #496

--- django/core/discourse.py
+++ django/core/discourse.py
@@ -8,17 +8,19 @@ logger = logging.getLogger(__name__)
 
 
 def create_discourse_user(user):
-    response = requests.post('{}/users'.format(settings.DISCOURSE_BASE_URL),
-                             data=dict(
-                                 name=user.get_full_name(),
-                                 username=user.username,
-                                 email=user.email,
-                                 password=''.join(str(c) for c in os.urandom(25)),
-                                 active=True,
-                             ),
-                             params=dict(
-                                 api_key=settings.DISCOURSE_API_KEY,
-                                 api_username=settings.DISCOURSE_API_USERNAME
-                             ))
-
+    response = requests.post(
+        f'{settings.DISCOURSE_BASE_URL}/users',
+        data=dict(
+            name=user.get_full_name(),
+            username=user.username,
+            email=user.email,
+            password=''.join(str(c) for c in os.urandom(25)),
+            active=True,
+        ),
+        headers={
+            'Content-Type': 'multipart/form-data;',
+            'Api-Key': settings.DISCOURSE_API_KEY,
+            'Api-Username': settings.DISCOURSE_API_USERNAME,
+        },
+    )
     return response
