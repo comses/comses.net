0e12274d5de6e046785d9c359a7b2e4ec0b0f4e8
Allen Lee <allen.lee@asu.edu>
2020-05-05 15:56:37 -0700

    fix: markdown component emits value for v-model
    otherwise changes to the component don't propagate back to the parent
    component's state
    
    removing autosave for now, will cause more trouble than its worth
    unless we start generating unique IDs for every markdown component

--- frontend/src/components/forms/markdown.ts
+++ frontend/src/components/forms/markdown.ts
@@ -1,53 +1,66 @@
 import BaseControl from '@/components/forms/base';
-import {Component, Prop} from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import Vue from 'vue';
 import VueSimplemde from 'vue-simplemde';
 import 'simplemde/dist/simplemde.min.css';
 
 Vue.component('vue-simplemde', VueSimplemde);
 
-
-enum ViewMode {
-    code,
-    view,
-    code_and_view,
-}
-
 @Component({
-    template: `<div :class="['form-group', {'child-is-invalid': isInvalid }]">
+  template: `<div :class="['form-group', {'child-is-invalid': isInvalid }]">
         <slot name="label" :label="label">
             <label :class="['form-control-label', requiredClass]">{{ label }}
                 <small class="ml-1 text-muted"><a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">Markdown</a> styling is supported</small>
             </label>        
         </slot>
-        <vue-simplemde :value="value" ref="markdownEditor"></vue-simplemde>
+        <vue-simplemde @input="emitValue" :configs="configs" :value="value" ref="markdownEditor"></vue-simplemde>
         <div v-if="isInvalid" class="invalid-feedback">{{ errorMessage }}</div>
         <slot name="help" :help="help">
-            <small class="mt-n4 form-text text-muted">{{ help }}</small>
         </slot>
     </div>`,
-    components: {
-        VueSimplemde
-    },
+  components: {
+    VueSimplemde
+  },
 })
-class MarkDown extends BaseControl {
-    @Prop({default: '10em'})
-    public minHeight: string;
+export default class Markdown extends BaseControl {
+  @Prop()
+  public label: string;
 
-    @Prop()
-    public label: string;
+  @Prop()
+  public help: string;
 
-    @Prop()
-    public help: string;
+  @Prop()
+  public value: string;
 
-    get simplemde() {
-        return (this.$refs.markdownEditor as any).simplemde;
+  get configs() {
+    return {
+      placeholder: this.help,
+      toolbar: [
+        "bold", "italic", "heading", "|",
+        "code", "quote", "unordered-list", "ordered-list", "|",
+        "horizontal-rule", "link", "image", "table", "|",
+        "preview", "side-by-side", "fullscreen", "|",
+        "guide"
+      ]
     }
+  }
 
-    public mounted() {
-        this.simplemde.codemirror.options.extraKeys.Tab = false;
-        this.simplemde.codemirror.options.extraKeys['Shift-Tab'] = false;
-    }
+  emitValue(value) {
+    this.$emit('input', value);
+  }
+
+  refresh() {
+    this.simplemde.codemirror.refresh();
+  }
+
+  get simplemde() {
+    return (this.$refs.markdownEditor as any).simplemde;
+  }
+
+  public mounted() {
+    const self = this;
+    const codemirror = this.simplemde.codemirror;
+    codemirror.options.extraKeys.Tab = false;
+    codemirror.options.extraKeys['Shift-Tab'] = false;
+  }
 }
-
-export default MarkDown;
