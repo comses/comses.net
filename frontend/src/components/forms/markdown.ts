50cf9cc07d96a99304399475cad891fdb1d97dad
Allen Lee <allen.lee@asu.edu>
2020-05-04 18:16:02 -0700

    fix: upgrade to vue-simplemde 1.0.4
    the base simplemde package itself is no longer maintained however, so we
    should look into using something like easymde and writing a vue wrapper
    for that

--- frontend/src/components/forms/markdown.ts
+++ frontend/src/components/forms/markdown.ts
@@ -1,6 +1,11 @@
 import BaseControl from '@/components/forms/base';
 import {Component, Prop} from 'vue-property-decorator';
-import {markdownEditor as MarkdownEditor} from 'vue-simplemde';
+import Vue from 'vue';
+import VueSimplemde from 'vue-simplemde';
+import 'simplemde/dist/simplemde.min.css';
+
+Vue.component('vue-simplemde', VueSimplemde);
+
 
 enum ViewMode {
     code,
@@ -11,17 +16,18 @@ enum ViewMode {
 @Component({
     template: `<div :class="['form-group', {'child-is-invalid': isInvalid }]">
         <slot name="label" :label="label">
-            <label :class="['form-control-label', requiredClass]">{{ label }}</label>
+            <label :class="['form-control-label', requiredClass]">{{ label }}
+                <small class="ml-1 text-muted"><a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">Markdown</a> styling is supported</small>
+            </label>        
         </slot>
-        <markdown-editor :value="value" @input="updateValue" ref="editor"></markdown-editor>
+        <vue-simplemde :value="value" ref="markdownEditor"></vue-simplemde>
         <div v-if="isInvalid" class="invalid-feedback">{{ errorMessage }}</div>
         <slot name="help" :help="help">
-            <small style='margin-top: -30px;' class="form-text text-muted"><a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">Markdown</a> styling is supported</small>
-            <small class="form-text text-muted">{{ help }}</small>
+            <small class="mt-n4 form-text text-muted">{{ help }}</small>
         </slot>
     </div>`,
     components: {
-        MarkdownEditor,
+        VueSimplemde
     },
 })
 class MarkDown extends BaseControl {
@@ -35,7 +41,7 @@ class MarkDown extends BaseControl {
     public help: string;
 
     get simplemde() {
-        return (this.$refs.editor as any).simplemde;
+        return (this.$refs.markdownEditor as any).simplemde;
     }
 
     public mounted() {
