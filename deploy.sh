9d293b9de68b6d1eec63c9be25533fbf21f51166
Allen Lee <allen.lee@asu.edu>
2020-05-05 18:49:54 -0700

    fix(build): improve staging deployment
    staging includes elasticsearch2 which wasn't getting updated when we
    bump versions
    
    [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,20 @@ set -o nounset
 ENVIRONMENT=${1:-"dev"}
 
 echo "Deploying latest build of comses.net for **${ENVIRONMENT}**"
-SERVICES="redis db elasticsearch nginx"
+SERVICES="redis db elasticsearch"
 git describe --tags >| django/release-version.txt;
 docker-compose build --pull;
 
 if [[ ${ENVIRONMENT} == "dev" ]]; then
-    SERVICES="redis db elasticsearch js"
+    # don't include elasticsearch2 or nginx in dev and bring up hot-reloading js server
+    SERVICES="${SERVICES} js"
 else
-    # bring js container up synchronously in staging + prod
-    docker-compose pull db redis nginx
-    docker-compose up js
-fi
-
-if [[ ${ENVIRONMENT} == "prod" ]]; then
+    # start js container synchronously in staging + prod and wait
+    # for the compiled webpack assets to be available
+    docker-compose pull db redis nginx;
+    docker-compose up js;
     SERVICES="${SERVICES} elasticsearch2"
 fi
 
 docker-compose up -d cms ${SERVICES};
-docker-compose exec cms inv prepare
+docker-compose exec cms inv prepare;
