0ad897137d09cb65c3697c5f63c72615f5fe8998
Allen Lee <allen.lee@asu.edu>
2020-09-08 16:35:02 -0700

    refine cite this model / private share URL UI
    refs #482

--- frontend/src/pages/codebase/release/regenerate_share_uuid.ts
+++ frontend/src/pages/codebase/release/regenerate_share_uuid.ts
@@ -5,29 +5,29 @@ import {api} from '@/api/connection';
 @Component({})
 class RegenerateShareUUID extends Vue {
     @Prop()
-    public absolute_url: string;
+    public absoluteUrl: string;
 
     @Prop()
-    public initial_share_url: string;
+    public initialShareUrl: string;
 
-    public share_url: string = '';
+    public shareUrl: string = '';
 
     public handle;
 
     public created() {
-        this.share_url = this.initial_share_url;
+        this.shareUrl = this.initialShareUrl;
     }
 
     public regenerateShareUuid() {
-        api.axios.post(`${this.absolute_url}regenerate_share_uuid/`)
-            .then((r) => this.share_url = r.data);
+        api.axios.post(`${this.absoluteUrl}regenerate_share_uuid/`)
+            .then((r) => this.shareUrl = r.data);
     }
 }
 
 const el = document.getElementById('regenerate_share_uuid');
 if (el) {
-    const initial_share_url = el.getAttribute('data-share-url');
-    const absolute_url = el.getAttribute('data-absolute-url');
-    new RegenerateShareUUID({propsData: {initial_share_url, absolute_url}})
+    const initialShareUrl = el.getAttribute('data-share-url');
+    const absoluteUrl = el.getAttribute('data-absolute-url');
+    new RegenerateShareUUID({propsData: {initialShareUrl, absoluteUrl}})
         .$mount(el);
 }
