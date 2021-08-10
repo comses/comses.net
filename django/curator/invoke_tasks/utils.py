5dc0f7e229266c394e8cf25e7276c7f2074454b8
Allen Lee <allen.lee@asu.edu>
2021-08-10 13:07:07 -0700

    fix: remove drupal_migrator

--- django/curator/invoke_tasks/utils.py
+++ django/curator/invoke_tasks/utils.py
@@ -8,7 +8,7 @@ env = {
     'python': 'python3',
     'project_name': 'cms',
     'project_conf': os.environ.get('DJANGO_SETTINGS_MODULE'),
-    'coverage_omit_patterns': ('drupal_migrator/*', 'templates/*', 'settings/*', 'migrations/*', 'wsgi.py', 'management/*', 'tasks.py', 'apps.py'),
+    'coverage_omit_patterns': ('templates/*', 'settings/*', 'migrations/*', 'wsgi.py', 'management/*', 'tasks.py', 'apps.py'),
     'coverage_src_patterns': ('home', 'library', 'core',),
 }
 
--- django/drupal_migrator/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrupalMigratorConfig(AppConfig):
-    name = 'drupal_migrator'
