0357ee06bed762628c27d2cc068ce06c2e59e676
Allen Lee <allen.lee@asu.edu>
2022-01-10 17:31:45 -0700

    fix: switch to vue-easymde
    guard against sentry init in development

--- frontend/src/components/forms/markdown.ts
+++ frontend/src/components/forms/markdown.ts
@@ -1,10 +1,10 @@
 import BaseControl from '@/components/forms/base';
 import { Component, Prop } from 'vue-property-decorator';
 import Vue from 'vue';
-import VueSimplemde from 'vue-simplemde';
-import 'simplemde/dist/simplemde.min.css';
+import VueEasymde from 'vue-easymde';
+import 'easymde/dist/easymde.min.css';
 
-Vue.component('vue-simplemde', VueSimplemde);
+Vue.component('vue-easymde', VueEasymde);
 
 @Component({
   template: `<div :class="['form-group', {'child-is-invalid': isInvalid }]">
@@ -13,13 +13,13 @@ Vue.component('vue-simplemde', VueSimplemde);
                 <small class="ml-1 text-muted"><a href="https://en.wikipedia.org/wiki/Markdown" target="_blank">Markdown</a> styling is supported</small>
             </label>
         </slot>
-        <vue-simplemde @input="emitValue" :configs="configs" :value="value" ref="markdownEditor"></vue-simplemde>
+        <vue-easymde @input="emitValue" :configs="configs" :value="value" ref="markdownEditor"></vue-easymde>
         <div v-if="isInvalid" class="invalid-feedback">{{ errorMessage }}</div>
         <slot name="help" :help="help">
         </slot>
     </div>`,
   components: {
-    VueSimplemde,
+    VueEasymde,
   },
 })
 export default class Markdown extends BaseControl {
@@ -49,18 +49,4 @@ export default class Markdown extends BaseControl {
     this.$emit('input', value);
   }
 
-  public refresh() {
-    this.simplemde.codemirror.refresh();
-  }
-
-  get simplemde() {
-    return (this.$refs.markdownEditor as any).simplemde;
-  }
-
-  public mounted() {
-    const self = this;
-    const codemirror = this.simplemde.codemirror;
-    codemirror.options.extraKeys.Tab = false;
-    codemirror.options.extraKeys['Shift-Tab'] = false;
-  }
 }
