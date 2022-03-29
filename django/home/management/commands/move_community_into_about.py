b2e0ccabcaafc990766d066933020c2c4066ad6c
Allen Lee <allen.lee@asu.edu>
2022-03-28 23:36:30 -0700

    fix: update residual about subnavigation links

--- django/home/management/commands/move_community_into_about.py
+++ django/home/management/commands/move_community_into_about.py
@@ -5,7 +5,7 @@ migrate community page from top level CategoryIndexPage into a child of the Abou
 from django.core.management.base import BaseCommand
 import logging
 
-from home.models import CategoryIndexPage, MarkdownPage, ConferenceIndexPage,
+from home.models import (CategoryIndexPage, ConferenceIndexPage, FaqPage, PeoplePage, ContactPage)
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +17,15 @@ building FAIR research software that helps us navigate understand our increasing
 complex world.
 """
 
+ABOUT_NAVIGATION_LINKS = (
+    ("About", "/about/"),
+    ("People", "/about/people/"),
+    ("Community", "/about/community/"),
+    ("FAQ", "/about/faq/"),
+    ("Contact", "/about/contact/"),
+)
+
+
 class Command(BaseCommand):
 
     """
@@ -24,7 +33,6 @@ class Command(BaseCommand):
     this can't be in a data migration for arcane wagtail reasons related to CategoryIndexPage subclassing
     wagtailcore.models.Page
     """
-
 
     def handle(self, *args, **options):
         community_page = CategoryIndexPage.objects.get(slug="community")
@@ -40,12 +48,34 @@ class Command(BaseCommand):
         community_page.breadcrumbs.all().delete()
 
         about_page = CategoryIndexPage.objects.get(slug='about')
-        about_page.add_child(instance=community_page)
+        community_page.move(about_page, pos='last-child')
         community_page.heading = 'CoMSES Net Community'
         community_page.description = DESCRIPTION
+        community_page.add_navigation_links(ABOUT_NAVIGATION_LINKS)
         community_page.save()
         # need to set navigation_links in every subsidiary page of the about page,
         # annoyingly. There should be a better way to do this in wagtail perhaps
         # using wagtailmenus
-
+        about_page.navigation_links.all().delete()
+        about_page.add_navigation_links(
+            ABOUT_NAVIGATION_LINKS
+        )
+        about_page.save()
+        # adjust navlinks for /about/people/
+        people_page = PeoplePage.objects.first()
+        people_page.navigation_links.all().delete()
+        people_page.add_navigation_links(
+            ABOUT_NAVIGATION_LINKS
+        )
+        people_page.save()
+        # FAQs
+        faq_page = FaqPage.objects.first()
+        faq_page.navigation_links.all().delete()
+        faq_page.add_navigation_links(ABOUT_NAVIGATION_LINKS)
+        faq_page.save()
+        # contact
+        contact_page = ContactPage.objects.first()
+        contact_page.navigation_links.all().delete()
+        contact_page.add_navigation_links(ABOUT_NAVIGATION_LINKS)
+        contact_page.save()
 
