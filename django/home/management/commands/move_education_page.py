afcd9d5eeaaaecca1095693ea88057cef2952344
Allen Lee <allen.lee@asu.edu>
2022-03-28 20:24:28 -0700

    feat: management command to lift education page
    Education page is to be promoted to the top level as a child of the Home
    page, out from its current Resources -> Education hierarchy
    
    TODO: add Education to top navbar and edit content

---
+++ django/home/management/commands/move_education_page.py
@@ -0,0 +1,53 @@
+"""
+Move education from
+
+"""
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from wagtail.core.models import Page
+
+from home.models import MarkdownPage, CategoryIndexPage
+
+logger = logging.getLogger(__name__)
+
+DESCRIPTION = """
+The Open Code badge is awarded to publications reliant on computational artifacts that archive the source code needed to reproduce their reported results in an open access, trusted digital repository that adheres to best practices for [software citation](https://www.force11.org/software-citation-principles) and [FAIR data](https://www.go-fair.org/fair-principles/).
+
+![open code html](/static/images/icons/open-code-badge.png)
+"""
+CONTENT = """
+In order to be awarded an Open Code badge, all source code should be made publicly available in a searchable, open access, trusted digital repository. A DOI, permanent URL or other permanent digital resource locator for accessing the model code must be provided that links to the specific version of the code used in the the publication. Narrative documentation and detailed software metadata should also accompany the source code and enable others to fully understand and/or replicate the source code. Metadata should clearly indicate all software, system, and data dependencies needed to compile and/or execute the source code with versions included. 
+
+As a concrete example, model source code made available on an author’s personal website, GitHub repository, or via email request does not meet the criteria for an Open Code badge whereas model source code published in the CoMSES Net Computational Model Library that has also passed the [CoMSES Net peer review process](/reviews/) does. For a comprehensive list of repositories where model source code can be archived to meet Open Code criteria please visit our [trusted digital repositories page](https://www.comses.net/resources/trusted-digital-repositories/).
+
+Journals participating in the Open Code program award Open Code badges to selected publications based on information provided by the authors and reviewers during the submission and review process. Online versions of publications should link the Open Code badge to a permanent digital resource locator that resolves to a landing page for the archived source code. If you have questions on the use of the Open Code badge, please [contact us](/about/contact/).
+
+These badges are derived from the [“Badges to Acknowledge Open Practices” project from the Open Science Framework](https://osf.io/tvyxz/) and are similarly licensed via the [CC-By Attribution 4.0](https://creativecommons.org/licenses/by/4.0/).
+"""
+
+
+class Command(BaseCommand):
+
+    """
+    Move existing Education page from a
+    """
+
+    def handle(self, *args, **options):
+        education_page = MarkdownPage.objects.get(slug='education')
+        home_page = Page.objects.get(slug='home')
+        education_page.heading = 'Educational Resources'
+        education_page.move(home_page, pos='last-child')
+        education_page.breadcrumbs.all().delete()
+        education_page.navigation_links.all().delete()
+        education_page.add_breadcrumbs(
+           (
+                ("Educational Resources", "/education/"),
+           )
+        )
+        education_page.save()
+        # remove education from resources subnavigation links
+        resources_page = CategoryIndexPage.objects.get(slug='resources')
+        resources_page.navigation_links.filter(url='/resources/education/').delete()
