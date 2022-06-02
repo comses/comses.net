9d02009bd712416c6c2eeef92107d20605aaba62
chrstngyn <Christine.N.Nguyen@asu.edu>
2022-06-02 10:44:29 -0700

    feat(WIP): create Vue component to capture demographic metadata
    include fields for email and reason for download

---
+++ frontend/src/components/Survey.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="modal fade"
+    id="surveyModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="surveyModalTitle"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="surveyModalTitle">Demographic Survey</h4>
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="inputEmail">Email</label>
+            </div>
+            <div class="form-group">
+              <label for="downloadReason">Reason for Download</label>
+              <select class="form-control" id="downloadReason"></select>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import * as _ from "lodash";
+
+@Component
+export default class Survey extends Vue {
+  // repopulate form with  fields
+  created() {}
+}
+</script>
