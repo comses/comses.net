e2a2babfb07404a924ee7a1ca79984f357eb9b54
Scott Foster <sgfost@asu.edu>
2022-09-02 17:25:10 -0700

    refactor: add custom select component

---
+++ frontend/src/components/forms/select.ts
@@ -0,0 +1,58 @@
+// TODO: consider refactoring form components to .vue SFCs
+import { Component, Prop } from 'vue-property-decorator';
+import BaseControl from './base';
+
+@Component({
+    template: `<div class="form-group">
+        <slot name="label">
+            <label :for='controlId' :class="['form-control-label', requiredClass]">{{ label }}</label>
+        </slot>
+        <select :id='controlId' :type="type" :name="name" :class="['form-control', {'is-invalid': isInvalid}]" 
+                :value="value" @change="updateValue($event.target.value)" v-model="selectedOption">
+            <option :value="option" :selected="option === selectedOption" v-for="option in options">
+                {{ option }}
+            </option>
+        </select>
+        <div v-if="customSelected()">
+            <label for="inputCustom" class="form-label"></label>
+            <input :value="value" class="form-control" id="inputCustom" :placeholder="customPlaceholder"
+            @input="updateValue($event.target.value)"/>
+        </div>
+        <div class="invalid-feedback">
+            {{ errorMessage }}
+        </div>
+        <slot name="help">
+            <small :aria-describedby='controlId' class="form-text text-muted">{{ help }}</small>
+        </slot>
+    </div>`, 
+})
+class Input extends BaseControl {
+    @Prop({ default: ''})
+    public validate: string;
+
+    @Prop({ default: 'text'})
+    public type: string;
+
+    @Prop({default: ''})
+    public label: string;
+
+    @Prop({ default: ''})
+    public help: string;
+
+    @Prop({ default: ['']})
+    public options: string[];
+
+    @Prop({ default: null})
+    public customOption: string | null;
+
+    @Prop({ default: ''})
+    public customPlaceholder: string;
+
+    public selectedOption = '';
+
+    public customSelected() {
+        return (this.customOption === this.selectedOption);
+    }
+}
+
+export default Input;
