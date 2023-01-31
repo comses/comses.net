d1d415dbb21353da982a15fa643c459ada4aa84f
Allen Lee <alee@users.noreply.github.com>
2023-01-30 21:18:38 -0700

    fix: add brute force contact form special casing

--- django/home/forms.py
+++ django/home/forms.py
@@ -1,9 +1,11 @@
 import logging
+import re
 
 from hcaptcha_field import hCaptchaField
 
 from django import forms
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.template import loader
 from django.utils.translation import ugettext_lazy as _
 from urllib.parse import urlparse
@@ -17,6 +19,8 @@ logger = logging.getLogger(__name__)
 
 
 class ContactForm(forms.Form):
+    SPAM_PATTERNS = r'(xevil|captchas)'
+    SPAM_EMAIL_DOMAINS = ('qq.com',)
     name = forms.CharField(max_length=100, label=_("Your name"))
     email = forms.EmailField(max_length=255, label=_("Your email address"))
     subject_text = forms.CharField(max_length=100, label=_("Message subject"))
@@ -43,6 +47,19 @@ class ContactForm(forms.Form):
             raise ValueError("Cannot get_context() from invalid contact form")
         site = Site.objects.first()
         return dict(self.cleaned_data, site=site)
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        for spam_domain in self.SPAM_EMAIL_DOMAINS:
+            if spam_domain in data:
+                raise ValidationError("Please try sending email from a supported email provider")
+        return data
+
+    def clean_body(self):
+        data = self.cleaned_data['body']
+        if re.search(self.SPAM_PATTERNS, data, re.IGNORECASE):
+            raise ValidationError("Your message was deemed invalid.")
+        return data
 
     @property
     def recipient_list(self):
