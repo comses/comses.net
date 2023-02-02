411d23e186a9c9f9ae21b4b072fa1bb51fe1e9f3
sgfost <sgfost@asu.edu>
2023-02-02 14:02:34 -0700

    fix: add vue SFC shim

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -3,6 +3,11 @@ declare module '*.html' {
     export default content;
 }
 
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
+
 declare const __BASE_URL__: string;
 
 declare module 'yup';
