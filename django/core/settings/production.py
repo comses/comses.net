e10a2d7a27065372290e7d1f9fce90ad06e64348
Allen Lee <alee@users.noreply.github.com>
2023-02-16 01:42:36 -0700

    fix: make csp hosts explicit

--- django/core/settings/production.py
+++ django/core/settings/production.py
@@ -23,19 +23,22 @@ SOCIALACCOUNT_PROVIDERS.pop("orcid", None)
 # https://django-csp.readthedocs.io/en/latest/index.html
 #
 # Full CSP Spec: https://www.w3.org/TR/CSP/
-CSP_DEFAULT_SRC = ("'self'", "*.comses.net", "www.google-analytics.com")
+CSP_DEFAULT_SRC = ("'self'", "comses.net", "www.comses.net", "forum.comses.net", "www.google-analytics.com")
 CSP_SCRIPT_SRC = (
     "'self'",
     "cdnjs.cloudflare.com",
     "browser.sentry-cdn.com",
     "www.googletagmanager.com",
-    "*.comses.net",
+    "www.google-analytics.com",
+    "forum.comses.net",
+    "www.comses.net",
+    "comses.net",
 )
 
 CSP_FONT_SRC = ("'self'", "fonts.googleapis.com", "fonts.gstatic.com")
 CSP_STYLE_SRC = ("'self'", "fonts.googleapis.com", "'unsafe-inline'")
 CSP_IMG_SRC = ("'self'", "data:", "i.ytimg.com")
-CSP_FRAME_SRC = ("'self'", "*.comses.net", "youtube.com", "www.youtube.com", "youtu.be" )
+CSP_FRAME_SRC = ("'self'", "comses.net", "forum.comses.net", "catalog.comses.net", , "www.comses.net", "youtube.com", "www.youtube.com", "youtu.be" )
 CSP_INCLUDE_NONCE_IN = ["script-src"]
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
