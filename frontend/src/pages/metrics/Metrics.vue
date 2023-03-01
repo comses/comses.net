a90afeaf1baf04df9d1d56d0bfedd96406af138c
sgfost <sgfost@asu.edu>
2023-02-28 17:14:14 -0700

    feat(WIP): add skeleton for metrics page
    Co-authored-by: BlllueSea <BlllueSea@users.noreply.github.com>
    Co-authored-by: Krisha100 <Krisha100@users.noreply.github.com>

---
+++ frontend/src/pages/metrics/Metrics.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>Hello world</h4>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class MetricsPage extends Vue {
+}
+</script>
+
+<style scoped></style>
