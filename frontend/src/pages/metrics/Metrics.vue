234bab559d9474579d6e54d0c0e69dd32095c235
Aiko Muraishi <kmuraish@asu.edu>
2023-03-02 18:09:03 -0700

    feat: add sample highcharts object in matrics page

--- frontend/src/pages/metrics/Metrics.vue
+++ frontend/src/pages/metrics/Metrics.vue
@@ -1,12 +1,32 @@
 <template>
-  <h4>Hello world</h4>
+  <div>
+      <highcharts :options="chartOptions"></highcharts>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Component, Vue } from "vue-property-decorator";
+import {HighchartsVue} from "highcharts-vue";
+import {Chart} from "highcharts-vue";
 
-@Component
+@Component({
+  // language=Vue
+  components: {
+    "highcharts": Chart,
+  },
+})
 export default class MetricsPage extends Vue {
+  chartOptions = {
+        // chart: {
+        //   type: 'spline'
+        // },
+        // title: {
+        //   text: 'Entire title'
+        // },
+        series: [{
+          data: [10, 0, 8, 2, 6, 4, 5, 5]
+        }]
+      };
 }
 </script>
 
