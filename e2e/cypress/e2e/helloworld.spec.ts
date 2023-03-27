05c009e5b0941faa0fd93f43d29382f4f530fe23
sgfost <sgfost@asu.edu>
2023-03-27 11:40:00 -0700

    fix: create e2e environment, disable workflow pending database seeding
    caused django tests to fail due to sharing the same settings that were
    modified to accomodate a pseudo-production mode

--- e2e/cypress/e2e/helloworld.spec.ts
+++ e2e/cypress/e2e/helloworld.spec.ts
@@ -1,9 +1,10 @@
-describe("Hello world component on the landing page", () => {
+describe("Hello world vue 3 component", () => {
   it("Should render and function correctly when clicking the button", { scrollBehavior: false }, () => {
-    cy.visit("/");
+    cy.visit("/codebases");
     cy.get("#app").within(() => {
       cy.get("h1")
         .should("contain", "Vue 3 Component");
+      cy.get("button").scrollIntoView()
       cy.get("button")
         .should("contain", "click me")
         .click();
