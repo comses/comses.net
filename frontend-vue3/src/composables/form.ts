370232d9126f580094c2ccbf668a71753dbc231d
sgfost <46429375+sgfost@users.noreply.github.com>
2023-04-06 17:01:25 -0700

    fix: turn off eslint no-unused-vars
    cleaned up where the rule found some style issues but otherwise its
    not particularly useful for development of vue components nor the requests api

--- frontend-vue3/src/composables/form.ts
+++ frontend-vue3/src/composables/form.ts
@@ -2,15 +2,14 @@ import { toRef } from "vue";
 import { yupResolver } from "@vorms/resolvers/yup";
 import { useField as useVField, useForm as useVForm, type UseFormOptions } from "@vorms/core";
 
-export function useField(props: any, fieldName: string) {
+export function useField(props: any, fieldNameProp: string="name") {
   /**
    * Wrapper for @vorms/core useField that provides a unique id for the field
    * Also takes care of converting to a ref
    */
 
-  const nameRef = toRef(props, "name");
+  const nameRef = toRef(props, fieldNameProp);
   const field = useVField(nameRef);
-  console.log(nameRef.value, field.value.value);
   const id = `form-field-${nameRef.value}`;
 
   return {
