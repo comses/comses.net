704620d557633a17eadc5a581091a846da90419a
sgfost <46429375+sgfost@users.noreply.github.com>
2023-04-13 14:07:21 -0700

    feat: add textarea form component

---
+++ frontend-vue3/src/components/form/FormTextArea.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <slot name="label">
+      <FormLabel v-if="label" :label="label" :id-for="id" :required="required" />
+    </slot>
+    <textarea
+      v-model="value"
+      :rows="rows"
+      :id="id"
+      v-bind="attrs"
+      :class="{ 'form-control': true, 'is-invalid': error }"
+    />
+    <slot name="help">
+      <FormHelp v-if="help" :help="help" :id-for="id" />
+    </slot>
+    <slot name="error">
+      <FormError v-if="error" :error="error" :id-for="id" />
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useField } from "@/composables/form";
+import FormLabel from "@/components/form/FormLabel.vue";
+import FormHelp from "@/components/form/FormHelp.vue";
+import FormError from "@/components/form/FormError.vue";
+
+export interface TextInputProps {
+  name: string;
+  label?: string;
+  help?: string;
+  placeholder?: string;
+  required?: boolean;
+  rows?: number;
+}
+
+const props = withDefaults(defineProps<TextInputProps>(), {
+  rows: 10,
+});
+
+const { id, value, attrs, error } = useField<string>(props, "name");
+</script>
