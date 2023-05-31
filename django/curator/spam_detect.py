d2782b512d47cf620c78678cc450d8fe2f301048
CharlesSheelam <csheelam@asu.edu>
2023-05-30 22:29:23 -0700

    fix: update user pipeline to fix latency issues

--- django/curator/spam_detect.py
+++ django/curator/spam_detect.py
@@ -11,21 +11,18 @@ warnings.filterwarnings("ignore") #ignore warnings
 class UserPipeline:
     def __init__(self):
         
-        self.columns = ['is_spam', 
-                        'first_name', 
-                        'last_name', 
-                        'is_active', 
-                        'email', 
-                        'timezone', 
-                        'industry', 
-                        'affiliations', 
-                        'bio', 
-                        'research_interests', 
-                        'personal_url', 
-                        'professional_url', 
-                        'spam_likely', #probability / confidence interval
-                        'user_id']
-        self.user_df = pd.DataFrame(columns=self.columns)
+        self.column_names = ['user__first_name',
+                'user__last_name',
+                'user__is_active',
+                'user__email',
+                'timezone',
+                'affiliations',
+                'bio',
+                'research_interests',
+                'personal_url',
+                'professional_url',
+                'user__id']
+        
 
     def custom_query_df(self, query_set):
         member_profiles = MemberProfile.objects.filter(**query_set)
@@ -46,47 +43,29 @@ class UserPipeline:
                     'research_interests': profile.research_interests,
                     'personal_url': profile.personal_url,
                     'professional_url': profile.professional_url,
-                    'user_id': int(i)
+                    'user_id': int(i) #use profile.user_id instead
                 }
                 custom_df = custom_df.append(row, ignore_index=True)
 
         return custom_df
 
 
-        
+    #load all users into a dataframe
     def all_users_df(self):
 
-        all_users = MemberProfile.objects.all()
-        for i, x in enumerate(all_users, start=1):
-            if x.user is None:
-                continue
-            else:
-
-                #populate df here:
-                row = {'first_name': x.user.first_name, 
-                       'last_name': x.user.last_name, 
-                       'is_active': x.is_active, 
-                       'email': x.email, 
-                       'timezone': x.timezone, 
-                       'affiliations': x.affiliations, 
-                       'bio': x.bio, 
-                       'research_interests': x.research_interests, 
-                       'personal_url': x.personal_url, 
-                       'professional_url': x.professional_url,
-                       'user_id' : int(i)
-                       }
-                
-                self.user_df = self.user_df.append(row, ignore_index=True)
-        
-        return self.user_df
-
+        df = pd.DataFrame(list(MemberProfile.objects.all().values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
+        return df
 
     #filter by date joined
     def users_joined_yesterday_df(self):
         
         yesterday = datetime.now() - timedelta(days=1)    
-        query = ({"user__date_joined__gte": yesterday})
-        df = self.custom_query_df(query)
+        query = {"user__date_joined__gte": yesterday}
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     
@@ -94,14 +73,18 @@ class UserPipeline:
         
         one_week_ago = datetime.now() - timedelta(days=7)    
         query = ({"user__date_joined__gte": one_week_ago})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     def users_joined_last_month_df(self):
        
         one_month_ago = datetime.now() - timedelta(days=31)    
         query = ({"user__date_joined__gte": one_month_ago})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
 
     def users_joined_last_year_df(self):
@@ -109,120 +92,128 @@ class UserPipeline:
        
         last_year = datetime.now() - timedelta(days=365)    
         query = ({"user__date_joined__gte": last_year})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
-    #ufilter by user name
+    #filter by first and last name
     def users_name_contains_df(self, search_string):
 
-        users_name_df = pd.DataFrame(columns=self.columns)
         member_profiles_first = MemberProfile.objects.filter(user__first_name__contains=search_string)
         member_profiles_last = MemberProfile.objects.filter(user__last_name__contains=search_string)
 
-        combined_member_profiles = list(chain(member_profiles_first, member_profiles_last))
+        combined_member_profiles = chain(member_profiles_first, member_profiles_last)
 
-        for i, x in enumerate(combined_member_profiles, start=1):
-            if x.user is None:
-                continue
-            else:
+        data = MemberProfile.objects.filter(pk__in=[profile.pk for profile in combined_member_profiles]).values(*self.column_names)
+        df = pd.DataFrame(data)
+        df['is_spam'] = None
+        df['spam_likely'] = None
 
-                #populate df here:
-                row = {'first_name': x.user.first_name, 
-                       'last_name': x.user.last_name, 
-                       'is_active': x.is_active, 
-                       'email': x.email, 
-                       'timezone': x.timezone, 
-                       'affiliations': x.affiliations, 
-                       'bio': x.bio, 
-                       'research_interests': x.research_interests, 
-                       'personal_url': x.personal_url, 
-                       'professional_url': x.professional_url,
-                       'user_id': int(i)
-                       }
-                
-                users_name_df = users_name_df.append(row, ignore_index=True)
-
-        return users_name_df
+        return df
     
     #filter by first name
     def users_first_name_exact_df(self, search_string):
 
         query = ({"user__first_name__exact": search_string})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
-    
     
     
     #filter by last name
     def users_last_name_exact_df(self, search_string):
 
         query = ({"user__last_name__exact": search_string})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     #filter by email
     def users_email_contains_df(self, search_string):
         
         query = ({"user__email__contains": search_string})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
         
     
     def users_email__exact_df(self, search_string):
         
         query = ({"user__email__exact": search_string})
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     # Filter by institution contains
     def users_institution_contains_df(self, search_string):
         query = {"institution__name__contains": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
 
     # Filter by exact institution
     def users_institution_exact_df(self, search_string):
         query = {"institution__name__exact": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     #filter by bio
     def users_bio_contains_df(self, search_string):
 
         query = {"bio__contains": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     def users_bio_exact_df(self, search_string):
 
         query = {"bio__exact": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     #filter by personal url
     def users_personal_url_contains_df(self, search_string):
 
         query = {"personal_url__contains": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     def users_personal_url_exact_df(self, search_string):
 
         query = {"personal_url__exact": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     #filter by professional url
     def users_professional_url_contains_df(self, search_string):
 
         query = {"professional_url__contains": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     def users_professional_url_exact_df(self, search_string):
 
         query = {"personal_url__exact": search_string}
-        df = self.custom_query_df(query)
+        df = pd.DataFrame(list(MemberProfile.objects.filter(**query).values(*self.column_names)))
+        df['is_spam'] = None
+        df['spam_likely'] = None
         return df
     
     
