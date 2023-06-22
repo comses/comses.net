18ce5f318ba491f065c13bbaaa1a286202162e63
Aiko Muraishi <blllue.sea.blllue@gmail.com>
2023-06-22 03:24:39 -0700

    fix/refactor: modifies UserPipeline functions and added an abstruct class SpamClassifier
    refactor: refactored UserMetadataSpamClassifier, integrate TextSpamClassifier, change filenames, UserPipeline to UserSpamStatusProcessor, and SpamRecommendation to UserSpamStatus

--- django/curator/management/commands/curator_spam_detection.py
+++ django/curator/management/commands/curator_spam_detection.py
@@ -4,62 +4,103 @@ import pathlib
 from django.core.management.base import BaseCommand
 
 from curator.models import TagCleanup, PENDING_TAG_CLEANUPS_FILENAME
-# from curator.spam_detection_models import SpamClassifier
-from curator.spam_detect import UserPipeline
-from curator.spam_detection_model import SpamClassifier
+
+# from curator.spam_detection_models import UserMetadataSpamClassifier
+from curator.spam_detection_models import SpamDetection
 
 logger = logging.getLogger(__name__)
 
-
 class Command(BaseCommand):
     help = "Perform spam detection"
-    pipeline = UserPipeline()
+    def __init__(self):
+        self.detection = SpamDetection()
+        self.processor = self.detection.processor
+        self.user_meta_classifier = self.detection.user_meta_classifier
+        self.text_classifier = self.detection.text_classifier
+
     def add_arguments(self, parser):
-        parser.add_argument("--init_table", "-i", action="store_true", default=False, help="initialize SpamRecommendation table. ")
-        parser.add_argument("--load", "-l", action="store_true", default=False, help="save initial dataset to the DB (SpamRecommendation table). ")
-        parser.add_argument("--train", "-t", action="store_true", default=False)
-        parser.add_argument("--predict", "-i", action="store_true", default=False)
-        parser.add_argument("--p_train", "-p", action="store_true", default=False, help="perform partial train on data with no ML model recomendation")
+        parser.add_argument(
+            "--exe",
+            "-e",
+            action="store_true",
+            default=False,
+            help="returns spam user_ids and model metrics",
+        )
+        parser.add_argument(
+            "--refine",
+            "-r",
+            action="store_true",
+            default=False,
+            help="retrain models and returns refined model metrics",
+        )
+        parser.add_argument(
+            "--load_labels",
+            "-l",
+            action="store_true",
+            default=False,
+            help="save initial dataset to the DB. ",
+        )
+        parser.add_argument("--train_user", "-tu", action="store_true", default=False)
+        parser.add_argument(
+            "--p_train_user",
+            "-ptu",
+            action="store_true",
+            default=False,
+            help="perform partial train on data with no ML model recomendation",
+        )
+        parser.add_argument("--predict_user", "-pu", action="store_true", default=False)
+        parser.add_argument("--train_text", "-tb", action="store_true", default=False)
+        parser.add_argument("--predict_text", "-pb", action="store_true", default=False)
 
-    def handle_init_table(self):
-        pipeline = UserPipeline()
-        pipeline.initalize_SpamRecommendation()
+    def handle_exe(self):
+        spam_users, user_model_metircs, text_model_metrics = self.detection.execute()
+        print(spam_users)
+        print(user_model_metircs)
+        print(text_model_metrics)
 
-    def handle_load(self, load_directory):
-        pipeline = UserPipeline()
-        pipeline.load_labels(load_directory)
+    def handle_refine(self):
+        user_model_metircs, text_model_metrics = self.detection.refine()
+        print(user_model_metircs)
+        print(text_model_metrics)
 
-    def handle_train(self):
-        classifier = SpamClassifier()
-        classifier.train()
+    def handle_load_labels(self, load_directory):
+        self.processor.update_labels(load_directory)
 
-    def handle_predict(self):
-        classifier = SpamClassifier()
-        classifier.predict()
+    def handle_train_user(self):
+        self.user_meta_classifier.fit()
 
-    def handle_p_train(self):
-        classifier = SpamClassifier()
-        classifier.partial_train()
+    def handle_predict_user(self):
+        self.user_meta_classifier.predict()
+
+    def handle_p_train_user(self):
+        self.user_meta_classifier.partial_fit()
+
+    def handle_train_text(self):
+        self.text_classifier.fit()
+
+    def handle_predict_text(self):
+        self.text_classifier.predict()
 
     def handle(self, *args, **options):
-        init_table = options["init_table"]
-        load = options["load"]
-        train = options["train"]
-        predict = options["predict"]
-        p_train = options["p_train"]
+        load = options["load_labels"]
+        train_user = options["train_user"]
+        predict_user = options["predict_user"]
+        p_train_user = options["p_train_user"]
+        train_text = options["train_text"]
+        predict_text = options["predict_text"]
 
-        # load_directory = pathlib.Path("/shared/incoming/curator/tags")
-        load_directory = pathlib.Path("/shared/curator/dataset.csv") #TODO check for the right place to store files
-        
-        if init_table:
-            self.handle_init_table()
+        # load_directory = pathlib.Path("/shared/curator/dataset.csv")
+        load_directory = pathlib.Path("dataset.csv")
+
         if load:
-            self.handle_load(load_directory)
-        elif train:
-            self.handle_train()
-        elif predict:
-            self.handle_predict()
-        elif p_train:
-            self.handle_p_train()
-
-
+            self.handle_load_labels(load_directory)
+        elif train_user:
+            self.handle_train_user()
+        elif predict_user:
+            self.handle_predict_user()
+        elif p_train_user:
+            self.handle_p_train_user()
+        elif train_text:
+            self.handle_train_text()
+        elif predict_text:
+            self.handle_predict_text()
