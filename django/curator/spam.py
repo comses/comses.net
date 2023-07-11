50881a1352ed85d0a515054479bceede080d1394
Aiko Muraishi <blllue.sea.blllue@gmail.com>
2023-07-11 09:59:32 -0700

    feat: added model metrics saving feature
    feat: added model validation in TextClassifier
    fix: fixed positional argument bug
    - fixed positional argument bug
    - dataset.csv replaced and create a directory in shared folder for spam detection related files
    
    fix: fix typing issue in df[labelled_by_curator] column
    - manual tests on curator_spam_detection management command passed only for UserMetadataSpamClassifier.

--- django/curator/spam.py
+++ django/curator/spam.py
@@ -11,7 +11,9 @@ from curator.models import UserSpamStatus
 from core.models import MemberProfile
 
 warnings.filterwarnings("ignore")  # ignore warnings
-DATASET_FILE_PATH = "dataset.csv"
+SPAM_DIR_PATH = "/shared/curator/spam/"
+DATASET_FILE_PATH = SPAM_DIR_PATH + "dataset.csv"
+
 
 class UserSpamStatusProcessor:
     """
@@ -33,19 +35,19 @@ class UserSpamStatusProcessor:
             "member_profile__personal_url",
             "member_profile__professional_url",
             "labelled_by_curator",
-            "labelled_by_bio_classifier",
+            "labelled_by_text_classifier",
             "labelled_by_user_classifier",
-            "bio_classifier_confidence",
+            "text_classifier_confidence",
             "user_classifier_confidence",
         ]
 
         self.type_int_bool_column_names = [
             "member_profile__user_id",
             "labelled_by_curator",
-            "labelled_by_bio_classifier",
-            "labelled_by_user_classifier",
-            "bio_classifier_confidence",
-            "user_classifier_confidence",
+            # "labelled_by_text_classifier",
+            # "labelled_by_user_classifier",
+            # "text_classifier_confidence",
+            # "user_classifier_confidence",
         ]
 
     def __rename_columns(self, df):
@@ -74,11 +76,14 @@ class UserSpamStatusProcessor:
             return df
         for col in df.columns:
             if col in self.type_int_bool_column_names:
-                df[col] = df[col].astype("float").astype("Int32")
+                df[col] = df[col].fillna(0).astype(int)
+                # It is safe to set Nan as ) because:
+                # for training, all values with labelled_by_curator=None are exclueded before passed to this function.
+                # for prediction, labelled_by_curator column is not used during prediction process.
             else:
                 df[col] = df[col].apply(
                     lambda text: re.sub(r"<.*?>", " ", str(text))
-                )  # Remove markdown
+                )  # Removing markdown
         return df
 
     def get_all_users_df(self):
@@ -124,33 +129,50 @@ class UserSpamStatusProcessor:
             )
         )
 
+    def get_unlabelled_users(self):
+        unlabelled_users = list(
+            UserSpamStatus.objects.filter(
+                Q(labelled_by_curator=None)
+                & Q(labelled_by_text_classifier=None)
+                & Q(labelled_by_user_classifier=None)
+            )
+        )
+        return unlabelled_users
+
     # FIXME: tune confidence threshold later
-    def get_spam_users(self, confidence_threshold=0.6):
+    def get_spam_users(self, confidence_threshold=0.5):
         """
         This functions will first filter out the users with labelled_by_curator==True,
         but the ones with None, only get users with labelled_by_user_classifier == True
-        or labelled_by_bio_classifier == True with a specific confidence level.
+        or labelled_by_text_classifier == True with a specific confidence level.
         """
         spam_users = list(
             UserSpamStatus.objects.filter(
                 Q(labelled_by_curator=True)
-                | Q(labelled_by_bio_classifier=True)
-                & Q(bio_classifier_confidence=confidence_threshold)
+                | Q(labelled_by_text_classifier=True)
+                & Q(text_classifier_confidence__gte=confidence_threshold)
                 | Q(labelled_by_user_classifier=True)
-                & Q(user_classifier_confidence_gte=confidence_threshold)
+                & Q(user_classifier_confidence__gte=confidence_threshold)
             )
         )
-        return spam_users  # (TODO: discuss about what should be returned)
+        return spam_users
 
     def update_labels(self, filepath=DATASET_FILE_PATH, check_DB=False):
         # This function updates "labelled_by_curator" field of the SpamRecommendation table bsed on external dataset file.
         # Dataset should have columns named "user_id" and "is_spam"
         # param : filepath of dataset to be loaded
         # return : None
-        if check_DB and UserSpamStatus.objects.filter(Q(labelled_by_curator=True) | Q(labelled_by_curator=False)).exists():
-            return 
-        
-        label_df = pd.read_csv(filepath)
+
+        if (
+            check_DB
+            and UserSpamStatus.objects.filter(
+                Q(labelled_by_curator=True) | Q(labelled_by_curator=False)
+            ).exists()
+        ):
+            # if there are user with non-None values as labelled_by_curator, no need to load the label file
+            return
+
+        label_df = pd.read_csv(filepath)  # TODO add exception
         for idx, row in label_df.iterrows():
             UserSpamStatus.objects.filter(
                 member_profile__user_id=row["user_id"]
@@ -165,15 +187,15 @@ class UserSpamStatusProcessor:
             ).update(is_training_data=True)
 
     def update_predictions(self, prediction_df, isTextClassifier=False):
-        # params : spam_recommendation_df ... a Dataframe with columns "user_id", "labelled_by_{}", and "{}_classifier_confidence"
+        # params : prediction_df ... a Dataframe with columns "user_id", "labelled_by_{}", and "{}_classifier_confidence"
         # return : None
         if isTextClassifier:
             for idx, row in prediction_df.iterrows():
                 UserSpamStatus.objects.filter(
                     member_profile__user_id=row.user_id
                 ).update(
-                    labelled_by_bio_classifier=row.labelled_by_bio_classifier,
-                    bio_classifier_confidence=row.bio_classifier_confidence,
+                    labelled_by_text_classifier=row.labelled_by_text_classifier,
+                    text_classifier_confidence=row.text_classifier_confidence,
                 )
         else:
             for idx, row in prediction_df.iterrows():
