d55b517237ab48b3cbb5637f195caef9bd169bce
CharlesSheelam <csheelam@asu.edu>
2024-01-24 20:28:03 -0700

    Fix: put ogp tag macros in 'common.jinja' and fix code repition

--- django/library/models.py
+++ django/library/models.py
@@ -625,8 +625,13 @@ class Codebase(index.Indexed, ClusterableModel):
         return urls
     
     def get_featured_rendition_url(self):
-        return self.get_featured_image().get_rendition('max-900x600').url
-    
+        url = self.get_featured_image().get_rendition('max-900x600').url
+
+        if url:
+            return url
+        else:
+            return None
+        
     def subpath(self, *args):
         return pathlib.Path(self.base_library_dir, *args)
 
