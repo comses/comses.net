6fc4801756dc12356b43a86d7bc55def9736602b
Allen Lee <alee@users.noreply.github.com>
2024-04-02 19:48:41 -0700

    fix: use correct imports and minor refactoring
    switch back to CodebaseRelease.codemeta + datacite properties, appending
    _metadata is more descriptive but feels a bit too smurf namey
    
    use codebase_release.codemeta attribute in tests in lieu of manually
    building things via the CodeMetaMetadata class
    
    clean up imports + minor hygiene

--- django/library/tests/test_models.py
+++ django/library/tests/test_models.py
@@ -1,6 +1,5 @@
 import logging
 import pathlib
-import random
 import semver
 import uuid
 
@@ -15,7 +14,7 @@ from .base import (
     ReleaseContributorFactory,
     ReleaseSetup,
 )
-from ..models import Codebase, CodebaseRelease, Contributor, License
+from ..models import Codebase, CodebaseRelease, License
 
 logger = logging.getLogger(__name__)
 
@@ -88,3 +87,155 @@ class CodebaseTest(BaseModelTestCase):
         # check file contents
         review_draft_sip_contents = review_draft.get_fs_api().list_sip_contents()
         self.assertEqual(source_sip_contents, review_draft_sip_contents)
+
+
+class CodebaseReleaseTest(BaseModelTestCase):
+    def get_perm_str(self, perm_prefix):
+        return "{}.{}_{}".format(
+            CodebaseRelease._meta.app_label,
+            perm_prefix,
+            CodebaseRelease._meta.model_name,
+        )
+
+    def setUp(self):
+        self.user_factory = UserFactory()
+        self.submitter = self.user_factory.create()
+        codebase_factory = CodebaseFactory(submitter=self.submitter)
+        self.codebase = codebase_factory.create()
+        self.codebase_release = self.codebase.create_release(initialize=False)
+
+    def test_anonymous_user_perms(self):
+        anonymous_user = AnonymousUser()
+        self.assertFalse(anonymous_user.has_perm(self.get_perm_str("add")))
+        self.assertFalse(
+            anonymous_user.has_perm(
+                self.get_perm_str("change"), obj=self.codebase_release
+            )
+        )
+        self.assertFalse(
+            anonymous_user.has_perm(
+                self.get_perm_str("delete"), obj=self.codebase_release
+            )
+        )
+        self.assertFalse(
+            anonymous_user.has_perm(
+                self.get_perm_str("view"), obj=self.codebase_release
+            )
+        )
+        self.codebase_release.status = CodebaseRelease.Status.PUBLISHED
+        self.codebase_release.save()
+        self.assertTrue(
+            anonymous_user.has_perm(
+                self.get_perm_str("view"), obj=self.codebase_release
+            )
+        )
+
+    def test_submitter_perms(self):
+        submitter = self.submitter
+        self.assertTrue(
+            submitter.has_perm(self.get_perm_str("change"), obj=self.codebase_release)
+        )
+        self.assertTrue(
+            submitter.has_perm(self.get_perm_str("delete"), obj=self.codebase_release)
+        )
+        self.assertTrue(
+            submitter.has_perm(self.get_perm_str("view"), obj=self.codebase_release)
+        )
+
+    def test_superuser_perms(self):
+        superuser = self.user_factory.create(is_superuser=True)
+        self.assertTrue(superuser.has_perm(self.get_perm_str("add")))
+        self.assertTrue(
+            superuser.has_perm(self.get_perm_str("change"), obj=self.codebase_release)
+        )
+        self.assertTrue(
+            superuser.has_perm(self.get_perm_str("delete"), obj=self.codebase_release)
+        )
+        self.assertTrue(
+            superuser.has_perm(self.get_perm_str("view"), obj=self.codebase_release)
+        )
+
+    def test_regular_user_perms(self):
+        regular_user = self.user_factory.create()
+        self.assertTrue(regular_user.has_perm(self.get_perm_str("add")))
+        self.assertFalse(
+            regular_user.has_perm(
+                self.get_perm_str("change"), obj=self.codebase_release
+            )
+        )
+        self.assertFalse(
+            regular_user.has_perm(
+                self.get_perm_str("delete"), obj=self.codebase_release
+            )
+        )
+        self.assertFalse(
+            regular_user.has_perm(self.get_perm_str("view"), obj=self.codebase_release)
+        )
+        self.codebase_release.status = CodebaseRelease.Status.PUBLISHED
+        self.codebase_release.save()
+        self.assertTrue(
+            regular_user.has_perm(self.get_perm_str("view"), obj=self.codebase_release)
+        )
+
+    def test_version_number_mutation(self):
+        other_codebase_release = self.codebase.create_release(initialize=False)
+        version_numbers = other_codebase_release.get_allowed_version_numbers()
+        self.assertEqual(
+            version_numbers,
+            set([semver.parse_version_info(vn) for vn in {"1.0.1", "1.1.0", "2.0.0"}]),
+        )
+
+        with self.assertRaises(ValidationError):
+            other_codebase_release.set_version_number("1.0.0")
+
+        with self.assertRaises(ValidationError):
+            other_codebase_release.set_version_number("foo-1.0.0")
+
+        other_codebase_release.set_version_number("54.2.0")
+        self.assertEqual(other_codebase_release.version_number, "54.2.0")
+
+        other_codebase_release.set_version_number("1.0.1")
+        self.assertEqual(other_codebase_release.version_number, "1.0.1")
+
+    def test_create_codebase_release_share_uuid(self):
+        """Ensure we can create a second codebase release an it hasa different share uuid"""
+        self.codebase_release.share_uuid = uuid.uuid4()
+        self.codebase_release.save()
+        cr = self.codebase.create_release(initialize=False)
+        self.assertNotEqual(self.codebase_release.share_uuid, cr.share_uuid)
+
+    def test_metadata_completeness(self):
+        # make sure release contributors are empty since we currently automatically add the submitter as an author
+        self.codebase_release.contributors.all().delete()
+        self.assertFalse(self.codebase_release.contributors.exists())
+
+        self.assertRaises(
+            ValidationError, lambda: self.codebase_release.validate_publishable()
+        )
+        self.codebase_release.os = "Windows"
+        self.assertRaises(
+            ValidationError, lambda: self.codebase_release.validate_publishable()
+        )
+
+        license = License.objects.create(
+            name="0BSD", url="https://spdx.org/licenses/0BSD.html"
+        )
+        self.codebase_release.license = license
+        self.assertRaises(
+            ValidationError, lambda: self.codebase_release.validate_publishable()
+        )
+
+        self.codebase_release.programming_languages.add("Java")
+        self.assertRaises(
+            ValidationError, lambda: self.codebase_release.validate_publishable()
+        )
+
+        release_contributor_factory = ReleaseContributorFactory(self.codebase_release)
+        contributor_factory = ContributorFactory(user=self.submitter)
+        contributor = contributor_factory.create()
+        release_contributor_factory.create(contributor)
+
+        self.assertRaises(
+            ValidationError, lambda: self.codebase_release.validate_publishable()
+        )
+        self.assertTrue(self.codebase_release.validate_metadata())
