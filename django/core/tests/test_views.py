017288586320503cfff8063c7fd55784ee0a1831
sgfost <sgfost@asu.edu>
2024-05-20 14:19:41 -0700

    test: add coverage for spam detection
    - refactor some test [Model]Factory classes to inherit common
      functionality + add method for returning data to make a create/update
      request

--- django/core/tests/test_views.py
+++ django/core/tests/test_views.py
@@ -1,13 +1,17 @@
+import json
 import logging
 
+from django.conf import settings
 from rest_framework.test import APIClient
 
 from django.urls import reverse
+from django.utils import timezone
 from django.test import TestCase
 
 from core.tests.base import UserFactory
 from core.tests.permissions_base import BaseViewSetTestCase
 from core.views import EventViewSet, JobViewSet
+from core.models import Job, Event
 from .base import JobFactory, EventFactory
 
 logger = logging.getLogger(__name__)
@@ -130,3 +134,100 @@ class ProfilePageRenderTestCase(TestCase):
     def test_list(self):
         response = self.client.get(reverse("core:profile-list"))
         self.assertEqual(response.status_code, 200)
+
+
+class SpamDetectionTestCase(BaseViewSetTestCase):
+
+    def setUp(self):
+        self.user_factory = UserFactory()
+        self.submitter = self.user_factory.create(username="submitter")
+        self.client.login(
+            username=self.submitter.username, password=self.user_factory.password
+        )
+        self.event_factory = EventFactory(submitter=self.submitter)
+        self.job_factory = JobFactory(submitter=self.submitter)
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_event_creation_with_honeypot_spam(self):
+        data = self.event_factory.get_request_data(honeypot_value="spammy content")
+        response = self.client.post(
+            reverse("core:event-list"),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        self.assertResponseCreated(response)
+        event = Event.objects.get(title=data["title"])
+        self.assertTrue(event.is_marked_spam)
+        self.assertIsNotNone(event.spam_content)
+        self.assertEqual(event.spam_content.detection_method, "honeypot")
+
+    def test_job_creation_with_timer_spam(self):
+        data = self.job_factory.get_request_data(
+            seconds_delta=settings.SPAM_LIKELY_SECONDS_THRESHOLD - 1
+        )
+        response = self.client.post(
+            reverse("core:job-list"),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        self.assertResponseCreated(response)
+        job = Job.objects.get(title=data["title"])
+        self.assertTrue(job.is_marked_spam)
+        self.assertIsNotNone(job.spam_content)
+        self.assertEqual(job.spam_content.detection_method, "form_submit_time")
+
+    def test_event_creation_without_spam(self):
+        data = self.event_factory.get_request_data()
+        response = self.client.post(
+            reverse("core:event-list"),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        self.assertResponseCreated(response)
+        event = Event.objects.get(title=data["title"])
+        self.assertFalse(event.is_marked_spam)
+        self.assertIsNone(event.spam_content)
+
+    def test_job_update_with_spam(self):
+        data = self.job_factory.get_request_data()
+        response = self.client.post(
+            reverse("core:job-list"),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        self.assertResponseCreated(response)
+        job = Job.objects.get(title=data["title"])
+        self.assertFalse(job.is_marked_spam)
+        self.assertIsNone(job.spam_content)
+        data = self.job_factory.get_request_data(
+            honeypot_value="spammy content",
+            seconds_delta=settings.SPAM_LIKELY_SECONDS_THRESHOLD + 1,
+        )
+        response = self.client.put(
+            job.get_absolute_url(),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        job.refresh_from_db()
+        self.assertTrue(job.is_marked_spam)
+        self.assertIsNotNone(job.spam_content)
+        self.assertEqual(job.spam_content.detection_method, "honeypot")
+
+    def test_exclude_spam_from_public_views(self):
+        data = self.event_factory.get_request_data(honeypot_value="spammy content")
+        response = self.client.post(
+            reverse("core:event-list"),
+            data,
+            HTTP_ACCEPT="application/json",
+            format="json",
+        )
+        public_url = reverse("core:event-list")
+        response = self.client.get(public_url)
+        self.assertNotContains(response, data["title"])
--- django/curator/tests/test_views.py
+++ django/curator/tests/test_views.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
 
-from core.tests.base import UserFactory
+from core.models import Event, SpamContent
+from core.tests.base import JobFactory, UserFactory, EventFactory
+from core.tests.permissions_base import BaseViewSetTestCase
 from curator.models import TagCleanup
 from curator.wagtail_hooks import TagCleanupPermissionHelper
 
@@ -50,3 +54,52 @@ class ProcessTagCleanupsTestCase(TestCase):
         tag_cleanup = TagCleanup.objects.get(new_name="bar")
         self.assertIsNotNone(tag_cleanup.transaction_id)
         self.assertFalse(ph.user_can_delete_obj(self.superuser, tag_cleanup))
+
+
+class SpamAdminViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user_factory = UserFactory()
+        self.superuser = self.user_factory.create(username="admin", is_superuser=True)
+        self.submitter = self.user_factory.create(username="submitter")
+        self.client.login(
+            username=self.superuser.username, password=self.user_factory.password
+        )
+        self.event_factory = EventFactory(submitter=self.submitter)
+        self.job_factory = JobFactory(submitter=self.submitter)
+        self.event = self.event_factory.create(
+            title="pool cleaning in Salzburg", submitter=self.submitter
+        )
+        SpamContent.objects.create(
+            content_object=self.event,
+            object_id=self.event.id,
+            detection_method="honeypot",
+            detection_details={"honeypot_value": "and hot tubs"},
+        )
+        self.event.refresh_from_db()
+
+    def tearDown(self):
+        self.client.logout()
+
+    def test_admin_confirm_spam(self):
+        url = reverse("curator:confirm_spam", args=[self.event.spam_content.id])
+        response = self.client.get(url)
+        self.event.refresh_from_db()
+        self.assertEqual(self.event.spam_content.status, SpamContent.Status.CONFIRMED)
+        self.assertTrue(self.event.is_marked_spam)
+        self.assertTrue(self.event.submitter.is_active)
+
+    def test_admin_reject_spam(self):
+        url = reverse("curator:reject_spam", args=[self.event.spam_content.id])
+        response = self.client.get(url)
+        self.event.refresh_from_db()
+        self.assertEqual(self.event.spam_content.status, SpamContent.Status.REJECTED)
+        self.assertFalse(self.event.is_marked_spam)
+
+    def test_admin_confirm_spam_deactivate_user(self):
+        url = reverse("curator:confirm_spam", args=[self.event.spam_content.id])
+        response = self.client.get(url + "?deactivate_user=true")
+        self.event.refresh_from_db()
+        self.assertEqual(self.event.spam_content.status, SpamContent.Status.CONFIRMED)
+        self.assertTrue(self.event.is_marked_spam)
+        self.assertFalse(self.event.submitter.is_active)
