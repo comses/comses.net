d29f6e9dbf246378bc575176645b5e0f5c7fe086
Mona Wong <mona@sdsc.edu>
2024-07-03 12:51:57 -0700

    updated tests; added saving codebase and release; setting codebase identifier; removed unused imports (comses/planning#146)

--- django/library/tests/test_dataciteapi.py
+++ django/library/tests/test_dataciteapi.py
@@ -1,22 +1,10 @@
-import logging
-
-# import pathlib
-# import semver
-# import uuid
+import logging, string, uuid
 
 from django.conf import settings
+from django.utils.crypto import get_random_string
 
-# from django.contrib.auth.models import AnonymousUser
-# from rest_framework.exceptions import ValidationError
-
-from core.tests.base import UserFactory, BaseModelTestCase
-from .base import (
-    CodebaseFactory,
-    ContributorFactory,
-    ReleaseContributorFactory,
-    ReleaseSetup,
-)
-from ..models import Codebase, CodebaseRelease, License
+from core.tests.base import BaseModelTestCase
+from ..models import Codebase, CodebaseRelease
 from library.doi import DataCiteApi, doi_matches_pattern
 
 logger = logging.getLogger(__name__)
@@ -25,34 +13,31 @@ logger = logging.getLogger(__name__)
 class DataCiteApiTest(BaseModelTestCase):
     def setUp(self):
         super().setUp()
+        allowed_chars = string.ascii_lowercase + string.digits
+
+        self.dc = DataCiteApi(dry_run=False)
 
         # determine current server's datacite prefix
-        env = settings.DEPLOYMENT_ENVIRONMENT
+        env = settings.DEPLOY_ENVIRONMENT
         self.DATACITE_PREFIX = (
             "10.82853" if env.is_development or env.is_staging else "10.25937"
         )
 
-        self.dc = DataCiteApi(dry_run=False)
-
-        # create a codebase and 3 releases
+        # create a codebase (which do NOT automatically create a release)
         self.cb = Codebase.objects.create(
-            title="Test codebase",
+            title="Test codebase and releases to Datacite",
             description="Test codebase description",
-            identifier="cb",
+            identifier=uuid.uuid4(),
             submitter=self.user,
         )
-        self.release1 = self.cb.create_release()
-        self.release2 = self.cb.create_release(
-            {"title": self.cb.title + " (2nd release)"}
-        )
-        self.release3 = self.cb.create_release(
-            {"title": self.cb.title + " (3rd release)"}
-        )
 
-        # assign fake DOIs for releases
-        self.release1.doi = self.DATACITE_PREFIX + "/release1"
-        self.release2.doi = self.DATACITE_PREFIX + "release2"
-        self.release3.doi = self.DATACITE_PREFIX + "release3"
+        # create 3 releases with fake DOIs to test the parent/child/sibling
+        for i in [1, 2, 3]:
+            r = self.cb.create_release()
+            r.doi = (
+                self.DATACITE_PREFIX + "/test-" + get_random_string(4, allowed_chars)
+            )
+            r.save()
 
     def test_mint_new_doi_for_codebase(self):
         self.assertTrue(self.dc.heartbeat())
@@ -60,28 +45,40 @@ class DataCiteApiTest(BaseModelTestCase):
         reply = self.dc.mint_new_doi_for_codebase(self.cb)
         self.assertContains(reply, self.DATACITE_PREFIX + "/")
         self.assertTrue(doi_matches_pattern(reply))
+        self.cb.doi = reply
+        self.cb.save()
 
     def test_update_metadata_for_codebase(self):
         self.assertTrue(self.dc.heartbeat())
         self.assertEqual(self.DATACITE_PREFIX, settings.DATACITE_PREFIX)
-        self.title = self.title + " (updated)"
+        self.cb.title = self.cb.title + " (updated)"
         # reply = self.dc.update_metadata_for_codebase(self)
         # self.assertTrue(DataCiteApi._is_same_metadata(reply, self.datacite.metadata))
         self.assertTrue(self.dc.update_metadata_for_codebase(self))
+        self.cb.save()
 
     def test_mint_new_doi_for_release(self):
         self.assertTrue(self.dc.heartbeat())
         self.assertEqual(self.DATACITE_PREFIX, settings.DATACITE_PREFIX)
-        reply = self.dc.mint_new_doi_for_release(self.release1)
+        release = self.cb.releases.first()
+        reply = self.dc.mint_new_doi_for_release(release)
         self.assertContains(reply, self.DATACITE_PREFIX + "/")
         self.assertTrue(doi_matches_pattern(reply))
+        release.doi = reply
+        release.save()
 
+    """
+    Note updating the title will update title for codebase and all releases;
+    so instead we'll update the release note field for the first release
+    """
     def test_update_metadata_for_release(self):
         self.assertTrue(self.dc.heartbeat())
         self.assertEqual(self.DATACITE_PREFIX, settings.DATACITE_PREFIX)
-        self.release1.title = self.release1.title + " (updated)"
+        release = self.cb.releases.first()
+        release.release_notes = release.release_notes + " (updated)"
         # reply = self.dc.update_metadata_for_release(self.release1)
         # self.assertTrue(
         #    DataCiteApi._is_same_metadata(reply, self.release1.datacite.metadata)
         # )
-        self.assertTrue(self.dc.update_metadata_for_release(self.release1))
+        self.assertTrue(self.dc.update_metadata_for_release(release))
+        release.save()
