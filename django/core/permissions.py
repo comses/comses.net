0676ac4a72c1320c85ab207054f4dc51914888a2
Luke Dwiyono <ldweagle1@gmail.com>
2024-08-12 11:01:44 -0700

    Limit mark_spam action to Moderators or superusers
    Added a new permission class MarkSpamPermissions which currently checks if the user is a moderator or superuser.

--- django/core/permissions.py
+++ django/core/permissions.py
@@ -56,5 +56,7 @@ class MarkSpamPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
             return False
+        if request.user.is_superuser:
+            return True
         
-        return request.user.is_superuser
+        return request.user.groups.filter(name='Moderators').exists()
