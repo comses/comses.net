d95809f2478df6bad5b29626027b3aae68a01b0d
sgfost <sgfost@asu.edu>
2025-01-24 18:35:51 -0700

    feat: rework git integration models and github/fs apis
    * use installation access tokens for user repos instead of user access
      tokens. this is a more secure workflow
    * add GithubIntegrationAppInstallation model for recording app
      installations (this will need to be created/updated using webhooks)
    * CodebaseGitMirror/"mirror" now refers to the local git repository
    * ^ can have multiple CodebaseGitRemote's which keep track of all the
      information needed to push to/archive from remote repositories
    
    TODO: re-implement views

--- django/library/tasks.py
+++ django/library/tasks.py
@@ -11,61 +11,53 @@ logger = logging.getLogger(__name__)
 
 
 @db_task(retries=3, retry_delay=30)
-def mirror_codebase(codebase_id: int, private_repo=False):
-    """asynchronous task that mirrors a codebase to a remote Github repository"""
+def build_and_push_codebase_repo(codebase_id: int, private_repo=False):
     codebase = Codebase.objects.get(id=codebase_id)
     mirror = codebase.git_mirror
     if not mirror:
         raise ValueError("Codebase does not have a git mirror")
-    mirror.organization_login = settings.GITHUB_MODEL_LIBRARY_ORG_NAME
-    mirror.save()
 
     git_fs_api = CodebaseGitRepositoryApi(codebase)
     local_repo = git_fs_api.update_or_build()
 
-    gh_api = GithubApi(
-        codebase=codebase,
-        local_repo=local_repo,
-        repo_name=mirror.repository_name,
-        is_user_repo=False,
-        organization_login=mirror.organization_login,
-        user_access_token=mirror.user_access_token,
-        private_repo=private_repo,
-    )
-    repo = gh_api.get_or_create_repo()
-    mirror.remote_url = repo.html_url
-    gh_api.push(local_repo)
-    gh_api.create_releases(local_repo)
-    mirror.update_remote_releases()
+    for remote in mirror.remotes.filter(should_push=True):
+        gh_api = GithubApi(
+            codebase=codebase,
+            local_repo=local_repo,
+            remote=remote,
+        )
+        repo = gh_api.get_or_create_repo(private=private_repo)
+        remote.url = repo.html_url
+        push_summary = gh_api.push(local_repo)
+        remote.last_push_log = push_summary
+        remote.save()
+        gh_api.create_releases(local_repo)
 
 
 @db_task(retries=3, retry_delay=30)
-def update_mirrored_codebase(codebase_id: int):
-    """asynchronous task that updates a mirrored codebase by pushing new releases to Github"""
+def add_releases_and_push_codebase_repo(codebase_id: int):
     codebase = Codebase.objects.get(id=codebase_id)
     mirror = codebase.git_mirror
     if not mirror:
         raise ValueError("Codebase does not have a git mirror")
-    if not mirror.remote_url:
-        raise ValueError("Codebase git mirror does not have a remote url")
 
     git_fs_api = CodebaseGitRepositoryApi(codebase)
     local_repo = git_fs_api.append_releases()
-    gh_api = GithubApi(
-        codebase=codebase,
-        local_repo=local_repo,
-        repo_name=mirror.repository_name,
-        is_user_repo=bool(mirror.user_access_token),
-        organization_login=mirror.organization_login,
-        user_access_token=mirror.user_access_token,
-    )
-    gh_api.push(local_repo)
-    gh_api.create_releases(local_repo)
-    mirror.update_remote_releases()
+
+    for remote in mirror.remotes.filter(should_push=True):
+        gh_api = GithubApi(
+            codebase=codebase,
+            local_repo=local_repo,
+            remote=remote,
+        )
+        push_summary = gh_api.push(local_repo)
+        remote.last_push_log = push_summary
+        remote.save()
+        gh_api.create_releases(local_repo)
 
 
 @db_task(retries=3, retry_delay=30)
-def update_mirrored_release_metadata(release_id: int):
+def update_release_branch_and_push_codebase_repo(release_id: int):
     """asynchronous task that updates a SINGLE RELEASE BRANCH with any metadata changes
     that may have occurred.
 
@@ -76,22 +68,21 @@ def update_mirrored_release_metadata(release_id: int):
     mirror = codebase.git_mirror
     if not mirror:
         raise ValueError("Codebase does not have a git mirror")
-    if not mirror.remote_url:
-        raise ValueError("Codebase git mirror does not have a remote url")
 
     git_fs_api = CodebaseGitRepositoryApi(codebase)
     local_repo = git_fs_api.update_release_branch(release)
+
+    # local_repo will be None if no changes were made
     if local_repo:
-        gh_api = GithubApi(
-            codebase=codebase,
-            local_repo=local_repo,
-            repo_name=mirror.repository_name,
-            is_user_repo=bool(mirror.user_access_token),
-            organization_login=mirror.organization_login,
-            user_access_token=mirror.user_access_token,
-        )
-        gh_api.push(local_repo)
-        mirror.update_remote_releases()
+        for remote in mirror.remotes.filter(should_push=True):
+            gh_api = GithubApi(
+                codebase=codebase,
+                local_repo=local_repo,
+                remote=remote,
+            )
+            push_summary = gh_api.push(local_repo)
+            remote.last_push_log = push_summary
+            remote.save()
 
 
 @db_task(retries=1, retry_delay=30)
@@ -102,5 +93,5 @@ def update_fs_release_metadata(release_id: int):
     fs_api.rebuild(metadata_only=True)
     # if the release is published and the codebase has a git mirror,
     # update the metadata in the git repository
-    if release.is_published and codebase.git_mirror and codebase.git_mirror.remote_url:
-        update_mirrored_release_metadata(release_id)
+    if release.is_published and codebase.git_mirror:
+        update_release_branch_and_push_codebase_repo(release_id)
