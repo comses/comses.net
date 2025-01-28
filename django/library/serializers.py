a7df0f50e8fb3c43ea49f04e650797b6b82d6ab6
sgfost <sgfost@asu.edu>
2025-01-27 20:12:56 -0700

    feat: views/routing for github integration config

--- django/library/serializers.py
+++ django/library/serializers.py
@@ -30,6 +30,7 @@ from core.serializers import (
     RelatedUserSerializer,
 )
 from .models import (
+    CodebaseGitRemote,
     PeerReviewer,
     ReleaseContributor,
     Codebase,
@@ -475,6 +476,29 @@ class RelatedCodebaseSerializer(serializers.ModelSerializer, FeaturedImageMixin)
             "live",
             "peer_reviewed",
             "repository_url",
+        )
+
+
+class CodebaseGitRemoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CodebaseGitRemote
+        fields = (
+            "id",
+            "owner",
+            "repo_name",
+            "url",
+            "should_push",
+            "should_archive",
+            "is_user_repo",
+            "last_push_log",
+        )
+        read_only_fields = (
+            "id",
+            "owner",
+            "repo_name",
+            "url",
+            "is_user_repo",
+            "last_push_log",
         )
 
 
