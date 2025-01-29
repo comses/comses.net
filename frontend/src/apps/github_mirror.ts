a5f8df94ac55d779415c4195f0030e81dbe56da3
sgfost <sgfost@asu.edu>
2025-01-28 17:39:34 -0700

    feat: add github sync configuration page
    this replaces the simple modal form to give better control over the feature

--- frontend/src/apps/github_mirror.ts
+++
@@ -1,8 +0,0 @@
-import "vite/modulepreload-polyfill";
-
-import { createApp } from "vue";
-import GithubMirrorModal from "@/components/GithubMirrorModal.vue";
-import { extractDataParams } from "@/util";
-
-const props = extractDataParams("github-mirror", ["identifier", "defaultRepoName"]);
-createApp(GithubMirrorModal, props).mount("#github-mirror");
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -379,8 +379,9 @@ export interface GithubAccount {
   installationId?: number;
 }
 
-export interface GitIntegrationStatus {
+export interface GitHubAppInstallationStatus {
   githubAccount: GithubAccount | null;
+  connectUrl: string;
   installationUrl: string | null;
 }
 
@@ -392,6 +393,7 @@ export interface CodebaseGitRemote {
   shouldPush: boolean;
   shouldArchive: boolean;
   isUserRepo: boolean;
+  isActive: boolean;
   lastPushLog: string;
 }
 
