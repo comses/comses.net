113978870b6a38efc327512ba5a859ee9c40b198
CharlesSheelam <csheelam@asu.edu>
2025-04-28 02:55:22 -0700

    feat: animated background + updated other sections
    Co-authored-by: sgfost <sgfost@users.noreply.github.com>

--- django/core/static/js/navbar-scroll.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', ()=>{
-    const header = document.querySelector('.navbar');
-    
-    if(window.scrollY > 900){
-        header.classList.add('scrolled');
-    } else{
-        header.classList.remove('scrolled');
-    }
-});   
